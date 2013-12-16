109de2a7d499aca4c9a2db2ddb9de0c58251338c
Zhu, Bin <bin.zhu@sonymobile.com>
2013-12-16 13:58:24 +0800

    Skip ID3v2 header when sniffing FLAC format media file
    If a FLAC file contains an ID3v2 header the current FLAC extractor will
    not recognize it as a FLAC file. The FLAC reference decoder will locate
    and skip ID3v2 headers.
    
    To be able to sniff more FLAC files skip ID3v2 headers so that the FLAC
    extractor can locate the signature word to recognize such variants of
    FLAC files.
    
    Change-Id: I0276f1cf8a31c734207684c5507bc510bec0a00c

--- media/libstagefright/FLACExtractor.cpp
+++ media/libstagefright/FLACExtractor.cpp
@@ -843,12 +843,39 @@ bool SniffFLAC(
         const sp<DataSource> &source, String8 *mimeType, float *confidence,
         sp<AMessage> *)
 {
+    off64_t pos = 0;
+
+    // Follow the FLAC reference decoder and skip any ID3v2 headers that
+    // may be present in the beginning of the stream.
+    for (;;) {
+        uint8_t id3header[10];
+        if (source->readAt(pos, id3header, sizeof(id3header))
+                != (ssize_t)sizeof(id3header)) {
+            return false;
+        }
+
+        // Locate and skip potential ID3v2 headers
+        if (memcmp("ID3", id3header, 3)) {
+            break;
+        }
+
+        size_t len = sizeof(id3header) +
+            (((id3header[6] & 0x7f) << 21) |
+             ((id3header[7] & 0x7f) << 14) |
+             ((id3header[8] & 0x7f) << 7) |
+              (id3header[9] & 0x7f));
+        pos += len;
+
+        ALOGV("skipped ID3 tag, new starting offset is %lld (0x%016llx)",
+             pos, pos);
+    }
+
     // first 4 is the signature word
     // second 4 is the sizeof STREAMINFO
     // 042 is the mandatory STREAMINFO
     // no need to read rest of the header, as a premature EOF will be caught later
     uint8_t header[4+4];
-    if (source->readAt(0, header, sizeof(header)) != sizeof(header)
+    if (source->readAt(pos, header, sizeof(header)) != sizeof(header)
             || memcmp("fLaC\0\0\0\042", header, 4+4))
     {
         return false;
