0211024de8732096abb98f5a5b4090103b473d7c
Alex Luebs <aluebs@google.com>
2015-12-14 21:32:13 -0800

    Add multi-mic support for the noise suppression pre-processing effect
    Change-Id: I7cf8d5657a25206e0a07e7e343635f7cf23e6f89

--- media/libeffects/preprocessing/PreProcessing.cpp
+++ media/libeffects/preprocessing/PreProcessing.cpp
@@ -584,11 +584,38 @@ int NsGetParameter(preproc_effect_t  *effect __unused,
     return status;
 }
 
-int NsSetParameter (preproc_effect_t *effect __unused,
-                    void *pParam __unused,
-                    void *pValue __unused)
+int NsSetParameter (preproc_effect_t *effect, void *pParam, void *pValue)
 {
     int status = 0;
+    webrtc::NoiseSuppression *ns = static_cast<webrtc::NoiseSuppression *>(effect->engine);
+    uint32_t param = *(uint32_t *)pParam;
+    uint32_t value = *(uint32_t *)pValue;
+    switch(param) {
+        case NS_PARAM_LEVEL:
+            ns->set_level((webrtc::NoiseSuppression::Level)value);
+            ALOGV("NsSetParameter() level %d, status %d", value, status);
+            break;
+        case NS_PARAM_TYPE:
+            if (value == NS_TYPE_MULTI_CHANNEL) {
+                ns->set_level(webrtc::NoiseSuppression::kLow);
+                // TODO(aluebs): Make the geometry settable.
+                webrtc::Config config;
+                std::vector<webrtc::Point> geometry;
+                geometry.push_back(webrtc::Point(-0.03f, 0.f, 0.f));
+                geometry.push_back(webrtc::Point(-0.01f, 0.f, 0.f));
+                geometry.push_back(webrtc::Point(0.01f, 0.f, 0.f));
+                geometry.push_back(webrtc::Point(0.03f, 0.f, 0.f));
+                config.Set<webrtc::Beamforming>(
+                        new webrtc::Beamforming(true, geometry));
+                effect->session->apm->SetExtraOptions(config);
+                ALOGV("NsSetParameter() multi mic %d, status %d", value, status);
+            }
+            break;
+        default:
+            ALOGW("NsSetParameter() unknown param %08x value %08x", param, value);
+            status = -EINVAL;
+    }
+
     return status;
 }
 
@@ -597,6 +624,10 @@ void NsEnable(preproc_effect_t *effect)
     webrtc::NoiseSuppression *ns = static_cast<webrtc::NoiseSuppression *>(effect->engine);
     ALOGV("NsEnable ns %p", ns);
     ns->Enable(true);
+    webrtc::Config config;
+    std::vector<webrtc::Point> geometry;
+    config.Set<webrtc::Beamforming>(new webrtc::Beamforming(true, geometry));
+    effect->session->apm->SetExtraOptions(config);
 }
 
 void NsDisable(preproc_effect_t *effect)
@@ -604,6 +635,10 @@ void NsDisable(preproc_effect_t *effect)
     ALOGV("NsDisable");
     webrtc::NoiseSuppression *ns = static_cast<webrtc::NoiseSuppression *>(effect->engine);
     ns->Enable(false);
+    webrtc::Config config;
+    std::vector<webrtc::Point> geometry;
+    config.Set<webrtc::Beamforming>(new webrtc::Beamforming(false, geometry));
+    effect->session->apm->SetExtraOptions(config);
 }
 
 static const preproc_ops_t sNsOps = {
@@ -896,17 +931,6 @@ int Session_SetConfig(preproc_session_t *session, effect_config_t *config)
     ALOGV("Session_SetConfig sr %d cnl %08x",
          config->inputCfg.samplingRate, config->inputCfg.channels);
     int status;
-
-    // if at least one process is enabled, do not accept configuration changes
-    if (session->enabledMsk) {
-        if (session->samplingRate != config->inputCfg.samplingRate ||
-                session->inChannelCount != inCnl ||
-                session->outChannelCount != outCnl) {
-            return -ENOSYS;
-        } else {
-            return 0;
-        }
-    }
 
     // AEC implementation is limited to 16kHz
     if (config->inputCfg.samplingRate >= 32000 && !(session->createdMsk & (1 << PREPROC_AEC))) {
@@ -1290,8 +1314,7 @@ int PreProcessingFx_Process(effect_handle_t     self,
             }
             session->framesIn = 0;
         }
-        session->procFrame->samples_per_channel_ =
-                session->apmFrameCount * session->inChannelCount;
+        session->procFrame->samples_per_channel_ = session->apmFrameCount;
 
         effect->session->apm->ProcessStream(session->procFrame);
 
@@ -1802,8 +1825,7 @@ int PreProcessingFx_ProcessReverse(effect_handle_t     self,
             }
             session->framesRev = 0;
         }
-        session->revFrame->samples_per_channel_ =
-                session->apmFrameCount * session->inChannelCount;
+        session->revFrame->samples_per_channel_ = session->apmFrameCount;
         effect->session->apm->AnalyzeReverseStream(session->revFrame);
         return 0;
     } else {
