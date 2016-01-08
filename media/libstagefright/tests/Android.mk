e4b6ca286b176f01b9383f75c99fbb7609b5a49d
Dan Liang <dan.liang@intel.com>
2016-01-08 13:30:51 +0800

    Enable 64bit mediaserver
    Change-Id: I5ee7dc646dfd29436ffef5e391ff8fb25b1325b4
    Signed-off-by: Dan Liang <dan.liang@intel.com>
    Signed-off-by: Ivan Pustovit <gl_aosp@globallogic.com>

--- media/libstagefright/tests/Android.mk
+++ media/libstagefright/tests/Android.mk
@@ -35,7 +35,9 @@ LOCAL_C_INCLUDES := \
 LOCAL_CFLAGS += -Werror -Wall
 LOCAL_CLANG := true
 
+ifneq ($(BOARD_USE_64BITMEDIA),true)
 LOCAL_32_BIT_ONLY := true
+endif
 
 include $(BUILD_NATIVE_TEST)
 
--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -68,6 +68,9 @@ LOCAL_STATIC_LIBRARIES := \
 LOCAL_MULTILIB := $(AUDIOSERVER_MULTILIB)
 
 LOCAL_MODULE:= libaudioflinger
+ifneq ($(BOARD_USE_64BITMEDIA),true)
+LOCAL_32_BIT_ONLY := true
+endif
 
 LOCAL_SRC_FILES += \
     AudioWatchdog.cpp        \
