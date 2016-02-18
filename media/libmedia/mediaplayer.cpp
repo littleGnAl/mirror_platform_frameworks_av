4a458b3a281e27d33ab5b3c9e9b8e7f0b5ff632a
tao.pei <tao.pei@ck-telecom.com>
2016-02-18 19:46:53 +0800

    stagefright:remove unnecessary code
    Not conform to the principle of software design.
    In multi-thread, if Thread A call MediaPlayer::start()
    while thread B call MediaPlayer::isPlaying(),may cause problems.
    
    Change-Id: I130295ab4cdadfbe73066d3d73a893609359b914

--- media/libmedia/mediaplayer.cpp
+++ media/libmedia/mediaplayer.cpp
@@ -388,13 +388,6 @@ bool MediaPlayer::isPlaying()
         bool temp = false;
         mPlayer->isPlaying(&temp);
         ALOGV("isPlaying: %d", temp);
-        if ((mCurrentState & MEDIA_PLAYER_STARTED) && ! temp) {
-            ALOGE("internal/external state mismatch corrected");
-            mCurrentState = MEDIA_PLAYER_PAUSED;
-        } else if ((mCurrentState & MEDIA_PLAYER_PAUSED) && temp) {
-            ALOGE("internal/external state mismatch corrected");
-            mCurrentState = MEDIA_PLAYER_STARTED;
-        }
         return temp;
     }
     ALOGV("isPlaying: no active player");
