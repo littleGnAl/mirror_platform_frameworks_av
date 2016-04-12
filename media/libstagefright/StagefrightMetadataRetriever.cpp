d174d972dc3c6ab910e0c9fbf838824e05689cf1
Yogesh Agrawal <yagrawal@gpartner-nvidia.com>
2016-04-12 12:53:33 +0530

    StagefrightMetadataRetriever: Release MediaExtractor after retrieving metadata for audio only files
    For audio only files, all metadata is retrieved in parseMetaData(), and
    mExtractor can be freed.
    
    Change-Id: Ifc47e34e337210ddfce6ebb01a1967b81a972f88

--- media/libstagefright/StagefrightMetadataRetriever.cpp
+++ media/libstagefright/StagefrightMetadataRetriever.cpp
@@ -499,13 +499,11 @@ VideoFrame *StagefrightMetadataRetriever::getFrameAtTime(
 MediaAlbumArt *StagefrightMetadataRetriever::extractAlbumArt() {
     ALOGV("extractAlbumArt (extractor: %s)", mExtractor.get() != NULL ? "YES" : "NO");
 
-    if (mExtractor == NULL) {
-        return NULL;
-    }
-
     if (!mParsedMetaData) {
-        parseMetaData();
+        if (mExtractor == NULL)
+            return NULL;
 
+        parseMetaData();
         mParsedMetaData = true;
     }
 
@@ -517,13 +515,13 @@ MediaAlbumArt *StagefrightMetadataRetriever::extractAlbumArt() {
 }
 
 const char *StagefrightMetadataRetriever::extractMetadata(int keyCode) {
-    if (mExtractor == NULL) {
-        return NULL;
-    }
 
     if (!mParsedMetaData) {
-        parseMetaData();
+        if (mExtractor == NULL) {
+            return NULL;
+        }
 
+        parseMetaData();
         mParsedMetaData = true;
     }
 
@@ -728,6 +726,15 @@ void StagefrightMetadataRetriever::parseMetaData() {
     // To check whether the media file is drm-protected
     if (mExtractor->getDrmFlag()) {
         mMetaData.add(METADATA_KEY_IS_DRM, String8("1"));
+    }
+
+    if (mExtractor->countTracks() == 1) {
+        sp<MetaData> trackMeta = mExtractor->getTrackMetaData(0);
+        const char *trackMIME;
+        CHECK(trackMeta->findCString(kKeyMIMEType, &trackMIME));
+        if (!strncasecmp("audio/", trackMIME, 6)) {
+            mExtractor.clear();
+        }
     }
 }
 
