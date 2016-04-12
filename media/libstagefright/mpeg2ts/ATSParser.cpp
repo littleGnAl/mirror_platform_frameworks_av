2802edadcf4d2c78c71ede60c880d906f5ee5e59
aarti jadhav-gaikwad <agaikwad@gpartner-nvidia.com>
2016-04-12 15:28:49 +0530

    mpeg2ts: skip packet with transport_error_indicator set
    skip packet with transport_error_indicator set in ATS parser
    
    Change-Id: I1db692c2cd583bafc220bd1a0a467543d1c50915

--- media/libstagefright/mpeg2ts/ATSParser.cpp
+++ media/libstagefright/mpeg2ts/ATSParser.cpp
@@ -1459,7 +1459,8 @@ status_t ATSParser::parseTS(ABitReader *br, SyncEvent *event) {
     }
 
     if (br->getBits(1)) {  // transport_error_indicator
-        // silently ignore.
+        // silently ignore, skip packet
+        br->skipBits(br->numBitsLeft());
         return OK;
     }
 
