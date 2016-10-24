a9222ba80eb1c76335b076f124d2f53327df369c
Jerry Zhang <zhangjerry@google.com>
2016-10-24 12:10:41 -0700

    Refactored Mtp driver interface into multiple classes.
    Added new interface for FunctionFS. This allows most of
    the driver code to exist in userspace. The driver will
    automatically use FunctionFS if it is enabled for that
    device, otherwise it will default to the kernel driver.
    
    The intention is to eventually deprecate the kernel driver.
    
    Bug: 30976142
    Change-Id: I36b8d16ca254fddd995b3ea1bd3d37b0ff4a28f7
    Test: New automated tests for MtpFfsHandle, AsyncIO.
    Manual testing on each device.

--- media/mtp/MtpServer.h
+++ media/mtp/MtpServer.h
@@ -23,8 +23,12 @@
 #include "MtpEventPacket.h"
 #include "mtp.h"
 #include "MtpUtils.h"
+#include "IMtpHandle.h"
 
 #include <utils/threads.h>
+#include <queue>
+#include <memory>
+#include <mutex>
 
 namespace android {
 
@@ -34,9 +38,6 @@ class MtpStorage;
 class MtpServer {
 
 private:
-    // file descriptor for MTP kernel driver
-    int                 mFD;
-
     MtpDatabase*        mDatabase;
 
     // appear as a PTP device
@@ -56,9 +57,14 @@ private:
     MtpRequestPacket    mRequest;
     MtpDataPacket       mData;
     MtpResponsePacket   mResponse;
-    MtpEventPacket      mEvent;
+
+    // events are queued asynchronously
+    std::queue<std::unique_ptr<MtpEventPacket>>    mEventQueue;
+    std::mutex          mEventLock;
 
     MtpStorageList      mStorages;
+
+    static IMtpHandle*  sHandle;
 
     // handle for new object, set by SendObjectInfo and used by SendObject
     MtpObjectHandle     mSendObjectHandle;
@@ -90,7 +96,7 @@ private:
     Vector<ObjectEdit*>  mObjectEditList;
 
 public:
-                        MtpServer(int fd, MtpDatabase* database, bool ptp,
+                        MtpServer(MtpDatabase* database, bool ptp,
                                     int fileGroup, int filePerm, int directoryPerm);
     virtual             ~MtpServer();
 
@@ -100,6 +106,7 @@ public:
     void                addStorage(MtpStorage* storage);
     void                removeStorage(MtpStorage* storage);
 
+    static int          configure(bool usePtp);
     void                run();
 
     void                sendObjectAdded(MtpObjectHandle handle);
