ef4df9ee96c7822e2428fc4aea52f74cb8a9df8c
Mingwei Shi <mingwei.shi@intel.com>
2017-07-26 10:02:40 +0800

    Provide the search path for vendor top leve policy config
    Bug: None
    Test: Move /system/etc/parameter-framework to vendor partition
    Change-Id: I3a1c1cf22bf62f6a93a5e27fbe86d8ab8f17ad7c
    Signed-off-by: Mingwei Shi <mingwei.shi@intel.com>

--- services/audiopolicy/engineconfigurable/wrapper/ParameterManagerWrapper.cpp
+++ services/audiopolicy/engineconfigurable/wrapper/ParameterManagerWrapper.cpp
@@ -65,6 +65,8 @@ namespace audio_policy
 {
 const char *const ParameterManagerWrapper::mPolicyPfwDefaultConfFileName =
     "/etc/parameter-framework/ParameterFrameworkConfigurationPolicy.xml";
+const char *const ParameterManagerWrapper::mPolicyPfwVendorConfFileName =
+    "/vendor/etc/parameter-framework/ParameterFrameworkConfigurationPolicy.xml";
 
 template <>
 struct ParameterManagerWrapper::parameterManagerElementSupported<ISelectionCriterionInterface> {};
@@ -75,7 +77,11 @@ ParameterManagerWrapper::ParameterManagerWrapper()
     : mPfwConnectorLogger(new ParameterMgrPlatformConnectorLogger)
 {
     // Connector
-    mPfwConnector = new CParameterMgrPlatformConnector(mPolicyPfwDefaultConfFileName);
+    if (access(mPolicyPfwVendorConfFileName, R_OK) == 0) {
+        mPfwConnector = new CParameterMgrPlatformConnector(mPolicyPfwVendorConfFileName);
+    } else {
+        mPfwConnector = new CParameterMgrPlatformConnector(mPolicyPfwDefaultConfFileName);
+    }
 
     // Logger
     mPfwConnector->setLogger(mPfwConnectorLogger);
