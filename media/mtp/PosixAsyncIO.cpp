e23346054032cdabe802a96b22b102ef830e4b3f
Jerry Zhang <zhangjerry@google.com>
2018-03-14 19:03:22 -0700

    Switch to a thread pool for aio operations
    Creating a thread for each operation was
    slightly wasteful. This also makes traces
    easier to read.
    
    Test: Transfer files, view traces
    Bug: 76154677
    Change-Id: I6028f113b9e5a84f01fb020a209a4dccadfdece4

--- media/mtp/PosixAsyncIO.cpp
+++ media/mtp/PosixAsyncIO.cpp
@@ -15,42 +15,108 @@
  */
 
 #include <android-base/logging.h>
-#include <condition_variable>
 #include <memory>
-#include <mutex>
+#include <pthread.h>
 #include <queue>
+#include <thread>
 #include <unistd.h>
 
 #include "PosixAsyncIO.h"
 
 namespace {
 
-void read_func(struct aiocb *aiocbp) {
-    aiocbp->ret = TEMP_FAILURE_RETRY(pread(aiocbp->aio_fildes,
-                aiocbp->aio_buf, aiocbp->aio_nbytes, aiocbp->aio_offset));
-    if (aiocbp->ret == -1) aiocbp->error = errno;
+std::thread worker_thread;
+std::deque<struct aiocb*> global_work;
+bool suspended = true;
+int aiocb_refcount = 0;
+std::mutex global_lock;
+std::condition_variable global_cv;
+
+void work_func(void *) {
+    pthread_setname_np(pthread_self(), "AsyncIO work");
+    while (true) {
+        struct aiocb *aiocbp;
+        {
+            std::unique_lock<std::mutex> lk(global_lock);
+            global_cv.wait(lk, []{return global_work.size() > 0 || suspended;});
+            if (suspended)
+                return;
+            aiocbp = global_work.back();
+            global_work.pop_back();
+        }
+        CHECK(aiocbp->queued);
+        int ret;
+        if (aiocbp->read) {
+            ret = TEMP_FAILURE_RETRY(pread(aiocbp->aio_fildes,
+                    aiocbp->aio_buf, aiocbp->aio_nbytes, aiocbp->aio_offset));
+        } else {
+            ret = TEMP_FAILURE_RETRY(pwrite(aiocbp->aio_fildes,
+               aiocbp->aio_buf, aiocbp->aio_nbytes, aiocbp->aio_offset));
+        }
+        {
+            std::unique_lock<std::mutex> lk(aiocbp->lock);
+            aiocbp->ret = ret;
+            if (aiocbp->ret == -1) {
+                aiocbp->error = errno;
+            }
+            aiocbp->queued = false;
+        }
+        aiocbp->cv.notify_all();
+    }
 }
 
-void write_func(struct aiocb *aiocbp) {
-    aiocbp->ret = TEMP_FAILURE_RETRY(pwrite(aiocbp->aio_fildes,
-                aiocbp->aio_buf, aiocbp->aio_nbytes, aiocbp->aio_offset));
-    if (aiocbp->ret == -1) aiocbp->error = errno;
+int aio_add(struct aiocb *aiocbp) {
+    CHECK(!aiocbp->queued);
+    aiocbp->queued = true;
+    {
+        std::unique_lock<std::mutex> lk(global_lock);
+        global_work.push_front(aiocbp);
+    }
+    global_cv.notify_one();
+    return 0;
 }
 
 } // end anonymous namespace
 
+aiocb::aiocb() {
+    this->ret = 0;
+    {
+        std::unique_lock<std::mutex> lk(global_lock);
+        if (aiocb_refcount == 0) {
+            CHECK(global_work.size() == 0);
+            CHECK(suspended);
+            suspended = false;
+            worker_thread = std::thread(work_func, nullptr);
+        }
+        aiocb_refcount++;
+    }
+}
+
 aiocb::~aiocb() {
-    CHECK(!thread.joinable());
+    CHECK(!this->queued);
+    {
+        std::unique_lock<std::mutex> lk(global_lock);
+        CHECK(!suspended);
+        if (aiocb_refcount == 1) {
+            CHECK(global_work.size() == 0);
+            suspended = true;
+            lk.unlock();
+            global_cv.notify_one();
+            worker_thread.join();
+            lk.lock();
+        }
+        aiocb_refcount--;
+    }
 }
 
 int aio_read(struct aiocb *aiocbp) {
-    aiocbp->thread = std::thread(read_func, aiocbp);
-    return 0;
+    aiocbp->read = true;
+    return aio_add(aiocbp);
 }
 
 int aio_write(struct aiocb *aiocbp) {
-    aiocbp->thread = std::thread(write_func, aiocbp);
-    return 0;
+    aiocbp->read = false;
+    return aio_add(aiocbp);
 }
 
 int aio_error(const struct aiocb *aiocbp) {
@@ -64,7 +130,10 @@ ssize_t aio_return(struct aiocb *aiocbp) {
 int aio_suspend(struct aiocb *aiocbp[], int n,
         const struct timespec *) {
     for (int i = 0; i < n; i++) {
-        aiocbp[i]->thread.join();
+        {
+            std::unique_lock<std::mutex> lk(aiocbp[i]->lock);
+            aiocbp[i]->cv.wait(lk, [aiocbp, i]{return !aiocbp[i]->queued;});
+        }
     }
     return 0;
 }
