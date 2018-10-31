323027e9c49856023d7a791ec315c2aa46be9d55
Tomoharu Kasahara <tomoharu.kasahara@sony.com>
2018-10-31 09:10:12 +0900

    Make verbose logs configurable in AudioPolicyManager
    Make verbose logs in AudioPolicyManager configurable
    to make it possible to debug easier.
    The default for persist.log.tag.APM_AudioPolicyManager
    should be D.
    
    Bug: 113756665
    Test: Check if verbose log with adb shell setprop
    persist.log.tag.APM_AudioPolicyManager V
    Change-Id: I0129a07e3afa11634945763b8d976699ce13093e

--- services/audiopolicy/managerdefault/AudioPolicyManager.cpp
+++ services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -15,7 +15,9 @@
  */
 
 #define LOG_TAG "APM_AudioPolicyManager"
-//#define LOG_NDEBUG 0
+
+// You can enable VERBOSE logging as follows:
+// adb shell setprop persist.log.tag.APM_AudioPolicyManager V
 
 //#define VERY_VERBOSE_LOGGING
 #ifdef VERY_VERBOSE_LOGGING
@@ -49,6 +51,11 @@
 #include <Serializer.h>
 #include "TypeConverter.h"
 #include <policy.h>
+
+// Need to keep the log statements even in production builds
+// to enable VERBOSE logging dynamically.
+#undef ALOGV
+#define ALOGV(...) ALOG(LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
 
 namespace android {
 
@@ -4167,6 +4174,12 @@ status_t AudioPolicyManager::initialize() {
     if (mPrimaryOutput == 0) {
         ALOGE("Failed to open primary output");
         status = NO_INIT;
+    }
+
+    // Set log level for ALOGV to DEBUG if not set
+    char propValue[PROPERTY_VALUE_MAX];
+    if (!property_get("persist.log.tag.APM_AudioPolicyManager", propValue, NULL)) {
+        property_set("persist.log.tag.APM_AudioPolicyManager", "D");
     }
 
     updateDevicesAndOutputs();
