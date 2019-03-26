ce4b13f563b17d1cc7f430258398c33544e21499
Saketh Sathuvalli <saketh.sathuvalli@ittiam.com>
2019-03-26 18:48:54 +0530

    libeffects: Introduced on the fly allocation of memories in LVM module
    Replaced memtab method of memory allocations with on the fly
    allocations using calloc.
    
    Test: android.media.cts.AudioEffectTest
    Test: android.media.cts.AudioPreProcessingTest
    Test: android.media.cts.BassBoostTest
    Test: android.media.cts.EnvReverbTest
    Test: android.media.cts.EqualizerTest
    Test: android.media.cts.LoudnessEnhancerTest
    Test: android.media.cts.PresetReverbTest
    Test: android.media.cts.VirtualizerTest
    Test: android.media.cts.VisualizerTest
    Test: Tested using standalone application
    Bug: 129491957
    Change-Id: I72b89059871ea5bdc0a338ad18bf0d378051b1e3

--- media/libeffects/lvm/lib/StereoWidening/src/LVCS_Process.cpp
+++ media/libeffects/lvm/lib/StereoWidening/src/LVCS_Process.cpp
@@ -91,8 +91,7 @@ LVCS_ReturnStatus_en LVCS_Process_CS(LVCS_Handle_t              hInstance,
     }
 #endif
 
-    pScratch  = (LVM_FLOAT *) \
-                  pInstance->MemoryTable.Region[LVCS_MEMREGION_TEMPORARY_FAST].pBaseAddress;
+    pScratch  = (LVM_FLOAT *)pInstance->pScratch;
 
     /*
      * Check if the processing is inplace
