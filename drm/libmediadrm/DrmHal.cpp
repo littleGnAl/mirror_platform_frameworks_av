67759ba4101a79a61f0f1ea2f195189417968b91
Steven Moreland <smoreland@google.com>
2019-05-13 12:53:24 -0700

    listByInterface -> listManifestByInterface
    This does two things:
    - makes sure that HALs configured as lazy HALs will be retrieved
    - will detect bad manifest entries earlier
    
    Bug: 131703193
    Test: boot
    Change-Id: Ib8c19e07b5ba866c02b9f5a1687139fc959aedc7

--- drm/libmediadrm/DrmHal.cpp
+++ drm/libmediadrm/DrmHal.cpp
@@ -24,7 +24,7 @@
 #include <binder/IServiceManager.h>
 
 #include <android/hardware/drm/1.0/types.h>
-#include <android/hidl/manager/1.0/IServiceManager.h>
+#include <android/hidl/manager/1.2/IServiceManager.h>
 #include <hidl/ServiceManagement.h>
 
 #include <media/EventMetric.h>
@@ -310,10 +310,10 @@ void DrmHal::cleanup() {
 Vector<sp<IDrmFactory>> DrmHal::makeDrmFactories() {
     Vector<sp<IDrmFactory>> factories;
 
-    auto manager = hardware::defaultServiceManager();
+    auto manager = hardware::defaultServiceManager1_2();
 
     if (manager != NULL) {
-        manager->listByInterface(drm::V1_0::IDrmFactory::descriptor,
+        manager->listManifestByInterface(drm::V1_0::IDrmFactory::descriptor,
                 [&factories](const hidl_vec<hidl_string> &registered) {
                     for (const auto &instance : registered) {
                         auto factory = drm::V1_0::IDrmFactory::getService(instance);
@@ -324,7 +324,7 @@ Vector<sp<IDrmFactory>> DrmHal::makeDrmFactories() {
                     }
                 }
             );
-        manager->listByInterface(drm::V1_1::IDrmFactory::descriptor,
+        manager->listManifestByInterface(drm::V1_1::IDrmFactory::descriptor,
                 [&factories](const hidl_vec<hidl_string> &registered) {
                     for (const auto &instance : registered) {
                         auto factory = drm::V1_1::IDrmFactory::getService(instance);
