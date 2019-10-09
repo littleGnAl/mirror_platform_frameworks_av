37d8b3fb9886c914de25de3208ea2c40fa58d81b
jaishank <jaishankar.rajendran@intel.com>
2019-10-09 14:32:42 +0530

    Supported FMA Intrinsics in Audio Resampler FIR processing module
    Test: make vts -j
          run vts run vts-hal run vts-kernel
          tests/build_and_run_all_unit_tests.sh
    
    Change-Id: I997e0bb3dd1282a3c9ca5a270b57898558162b30
    Signed-off-by: jaishank <jaishankar.rajendran@intel.com>

--- media/libaudioprocessing/AudioResamplerFirOps.h
+++ media/libaudioprocessing/AudioResamplerFirOps.h
@@ -36,12 +36,17 @@ namespace android {
 #include <arm_neon.h>
 #endif
 
-#if defined(__SSSE3__)  // Should be supported in x86 ABI for both 32 & 64-bit.
-#define USE_SSE (true)
+#if defined(__AVX2__)  // Should be supported in x86 ABI for both 32 & 64-bit.
+#define USE_AVX2 (true)  // Inference AVX2/FMA Intrinsics
+#include <immintrin.h>
+#elif defined(__SSSE3__)  // Should be supported in x86 ABI for both 32 & 64-bit.
+#define USE_SSE (true)  // Inference SSE Intrinsics
 #include <tmmintrin.h>
 #else
 #define USE_SSE (false)
+#define USE_AVX2(false)
 #endif
+
 
 template<typename T, typename U>
 struct is_same
