4a44b0462b815d7760f627399d1a2f9c7c9f6e1d
François Gaffie <francois.gaffie@renault.com>
2019-11-18 15:50:22 +0100

    audiopolicy: apm: factorize internal patch creation/release
    This CL removes duplicated code for patch creation for
    -voice call
    -system API to create patches.
    -system API to start/stop AudioSource
    
    It creates internal methods to create/release Audio Patches.
    
    The benefit of this rework is to have startAudioSource now working
    whatever Sw or HW bridging is used.
    
    Test: Voice Call
    Bug:
    Change-Id: I82738d116abd94f7738fce8ca3a1c3466942060a
    Signed-off-by: François Gaffie <francois.gaffie@renault.com>

--- services/audiopolicy/managerdefault/AudioPolicyManager.h
+++ services/audiopolicy/managerdefault/AudioPolicyManager.h
@@ -856,6 +856,29 @@ private:
             param.addInt(String8(AudioParameter::keyMonoOutput), (int)mMasterMono);
             mpClientInterface->setParameters(output, param.toString());
         }
+
+        /**
+         * @brief createAudioPatchInternal internal function to manage audio patch creation
+         * @param[in] patch structure containing sink and source ports configuration
+         * @param[out] handle patch handle to be provided if patch installed correctly
+         * @param[out] uid of the client
+         * @param[out] delayMs if required
+         * @param[in,out] sourceDesc [optional] in case of external source, source client to be
+         * configured by the patch, i.e. assigning an Output (HW or SW)
+         * @return NO_ERROR if patch installed correclty, error code otherwise.
+         */
+        status_t createAudioPatchInternal(const struct audio_patch *patch,
+                                          audio_patch_handle_t *handle,
+                                          uid_t uid, uint32_t delayMs,
+                                          const sp<SourceClientDescriptor>& sourceDesc = nullptr);
+        /**
+         * @brief releaseAudioPatchInternal internal function to remove an audio patch
+         * @param handle of the patch to be removed
+         * @param[out] delayMs if required
+         * @return NO_ERROR if patch removed correclty, error code otherwise.
+         */
+        status_t releaseAudioPatchInternal(audio_patch_handle_t handle, uint32_t delayMs = 0);
+
         status_t installPatch(const char *caller,
                 audio_patch_handle_t *patchHandle,
                 AudioIODescriptorInterface *ioDescriptor,
