670278c3f6d2a7897f9bf4f4b0c58c521e0f21ef
Manisha Jajoo <manisha.jajoo@ittiam.com>
2020-02-06 18:35:27 +0530

    ExtractorUnitTest: Add support for random seek in SeekTest
    Bug: 148948209
    
    Test: ExtractorUnitTest -P /data/local/tmp/extractorRes/
    
    Change-Id: I53c4bf7dd6b734cc68d7c2c122fd9f701f4a17e0

--- media/extractors/tests/ExtractorUnitTest.cpp
+++ media/extractors/tests/ExtractorUnitTest.cpp
@@ -43,7 +43,9 @@ using namespace android;
 #define OUTPUT_DUMP_FILE "/data/local/tmp/extractorOutput"
 
 constexpr int32_t kMaxCount = 10;
-constexpr int32_t kOpusSeekPreRollUs = 80000;  // 80 ms;
+constexpr int32_t kOpusSeekPreRollUs = 80000;      // 80 ms;
+constexpr int32_t kRandomSeekToleranceUs = 40000;  // 40 ms;
+constexpr int32_t kRandomSeed = 700;
 
 static ExtractorUnitTestEnvironment *gEnv = nullptr;
 
@@ -166,6 +168,40 @@ int32_t ExtractorUnitTest::createExtractor() {
     }
     if (!mExtractor) return -1;
     return 0;
+}
+
+void randomSeekTest(MediaTrackHelper *track, int64_t clipDuration) {
+    int32_t status = 0;
+    int64_t seekToTimeStamp = 0;
+    int32_t seekCount = 0;
+    bool hasTimestamp = false;
+    srand(kRandomSeed);
+    do {
+        seekToTimeStamp = rand() % clipDuration;
+        MediaTrackHelper::ReadOptions *options = new MediaTrackHelper::ReadOptions(
+                CMediaTrackReadOptions::SEEK_CLOSEST | CMediaTrackReadOptions::SEEK,
+                seekToTimeStamp);
+        ASSERT_NE(options, nullptr) << "Cannot create read option";
+
+        MediaBufferHelper *buffer = nullptr;
+        status = track->read(&buffer, options);
+        if (buffer) {
+            AMediaFormat *metaData = buffer->meta_data();
+            int64_t timeStamp = 0;
+            hasTimestamp = AMediaFormat_getInt64(metaData, AMEDIAFORMAT_KEY_TIME_US, &timeStamp);
+            ASSERT_TRUE(hasTimestamp)
+                    << "Extractor didn't set timestamp for the given sample";
+
+            buffer->release();
+            EXPECT_LE(abs(timeStamp - seekToTimeStamp), kRandomSeekToleranceUs)
+                    << "Seek unsuccessful. Expected timestamp range ["
+                    << seekToTimeStamp - kRandomSeekToleranceUs << ", "
+                    << seekToTimeStamp + kRandomSeekToleranceUs << "] "
+                    << "received " << timeStamp;
+        }
+        delete options;
+        seekCount++;
+    } while (seekCount < kMaxCount);
 }
 
 void getSeekablePoints(vector<int64_t> &seekablePoints, MediaTrackHelper *track) {
@@ -380,11 +416,7 @@ TEST_P(ExtractorUnitTest, MultipleStartStopTest) {
 }
 
 TEST_P(ExtractorUnitTest, SeekTest) {
-    // Flac, Midi and Wav extractor can give samples from any pts and mark the given sample as
-    // sync frame. So, this seek test is not applicable to these extractors
-    if (mDisableTest || mExtractorName == FLAC || mExtractorName == WAV || mExtractorName == MIDI) {
-        return;
-    }
+    if (mDisableTest) return;
 
     ALOGV("Validates %s Extractor behaviour for different seek modes", GetParam().first.c_str());
     string inputFileName = gEnv->getRes() + GetParam().second;
@@ -417,6 +449,24 @@ TEST_P(ExtractorUnitTest, SeekTest) {
         MediaBufferGroup *bufferGroup = new MediaBufferGroup();
         status = cTrack->start(track, bufferGroup->wrap());
         ASSERT_EQ(OK, (media_status_t)status) << "Failed to start the track";
+
+        // Flac, Wav and Midi extractor can give samples from any pts and mark the given sample as
+        // sync frame. Hence we test for random seek for these extractors
+        if (mExtractorName == FLAC || mExtractorName == WAV || mExtractorName == MIDI) {
+            AMediaFormat *trackMeta = AMediaFormat_new();
+            ASSERT_NE(trackMeta, nullptr) << "AMediaFormat_new returned null AMediaformat";
+
+            status = mExtractor->getTrackMetaData(trackMeta, idx, 1);
+            ASSERT_EQ(OK, (media_status_t)status) << "Failed to get trackMetaData";
+
+            int64_t clipDuration = 0;
+            AMediaFormat_getInt64(trackMeta, AMEDIAFORMAT_KEY_DURATION, &clipDuration);
+            ASSERT_GT(clipDuration, 0) << "Invalid clip duration ";
+            randomSeekTest(track, clipDuration);
+            AMediaFormat_delete(trackMeta);
+            continue;
+        }
+
         getSeekablePoints(seekablePoints, track);
         ASSERT_GT(seekablePoints.size(), 0)
                 << "Failed to get seekable points for " << GetParam().first << " extractor";
