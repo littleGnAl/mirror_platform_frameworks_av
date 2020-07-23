d459682145c4ce85df28b5e93301896cfd2f5ace
Houxiang Dai <houxiang.dai@mediatek.com>
2020-07-23 14:07:22 +0800

    Add kWhatSetParameters/OMX_EventConfigUpdate
    [Description]
    handle kWhatSetParameters message in OutputPortSettingsChangedState
    handle OMX_EventConfigUpdate event in OutputPortSettingsChangedState
    
    In HDR10+ test, we have to associate each HDR10+ metadata to a particular
    frame. If receive a kWhatSetParameters message with "hdr10-plus-info"
    buffer, it should not be deferred in OutputPortSettingsChangedState and
    adopt OMX_SetConfig to associates this config with the next input buffer
    sent in OMX_EmptyThisBuffer. The OMX_EventConfigUpdate event report from
    component should be handled also in OutputPortSettingsChangedState,
    where is to associate updated "hdr10-plus-info" metadata with the next
    output buffer sent via FillBufferDone callback.
    
    Change-Id: I27e4614487414063831fa760b9e9ca96b1c3712c

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -8233,10 +8233,27 @@ bool ACodec::OutputPortSettingsChangedState::onMessageReceived(
             FALLTHROUGH_INTENDED;
         }
         case kWhatResume:
-        case kWhatSetParameters:
         {
             if (msg->what() == kWhatResume) {
                 ALOGV("[%s] Deferring resume", mCodec->mComponentName.c_str());
+            }
+
+            mCodec->deferMessage(msg);
+            handled = true;
+            break;
+        }
+
+        case kWhatSetParameters:
+        {
+            sp<AMessage> params;
+            CHECK(msg->findMessage("params", &params));
+
+            sp<ABuffer> hdr10PlusInfo;
+            if (params->findBuffer("hdr10-plus-info", &hdr10PlusInfo)
+                    && hdr10PlusInfo != nullptr && hdr10PlusInfo->size() > 0) {
+                (void)mCodec->setHdr10PlusInfo(hdr10PlusInfo);
+
+                params->removeEntryAt(params->findEntryByName("hdr10-plus-info"));
             }
 
             mCodec->deferMessage(msg);
@@ -8352,6 +8369,15 @@ bool ACodec::OutputPortSettingsChangedState::onOMXEvent(
             }
 
             return false;
+        }
+
+        case OMX_EventConfigUpdate:
+        {
+            CHECK_EQ(data1, (OMX_U32)kPortIndexOutput);
+
+            mCodec->onConfigUpdate((OMX_INDEXTYPE)data2);
+
+            return true;
         }
 
         default:
