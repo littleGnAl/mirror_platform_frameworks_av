ecbc6ea6a3bed314eb6bcfafd5aa5abb6497d501
Neelkamal Semwal <neelkamal.semwal@ittiam.com>
2020-07-30 14:52:11 +0530

    m4v_h263: Fix integer-overflow in decoder IDCT functions
    Bug: 151816603
    Bug: 155561853
    Bug: 151815723
    
    Test: POC in bug descriptions
    
    Change-Id: If891929807defd46754eb48b98307d0225a272b1

--- media/libstagefright/codecs/m4v_h263/dec/include/m4vh263_decoder_pv_types.h
+++ media/libstagefright/codecs/m4v_h263/dec/include/m4vh263_decoder_pv_types.h
@@ -29,6 +29,7 @@ typedef uint16_t uint16;
 typedef int16_t int16;
 typedef uint32_t uint32;
 typedef int32_t int32;
+typedef int64_t int64;
 typedef unsigned int uint;
 
 // Redefine the oscl memory management routines
