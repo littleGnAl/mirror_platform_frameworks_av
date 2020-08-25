850eba7e1aa28e582a5f7d28b6a923ff4278c9ae
Francois Gaffie <francois.gaffie@renault.com>
2020-08-25 14:41:07 +0200

    audiopolicy: get rid of stream volume APIs
    Test:
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: I389f2f7a04bfd00cf1945fe5d2f660293779d0a0

--- services/audiopolicy/service/AudioPolicyService.h
+++ services/audiopolicy/service/AudioPolicyService.h
@@ -108,15 +108,8 @@ public:
     virtual status_t startInput(audio_port_handle_t portId);
     virtual status_t stopInput(audio_port_handle_t portId);
     virtual void releaseInput(audio_port_handle_t portId);
-    virtual status_t initStreamVolume(audio_stream_type_t stream,
-                                      int indexMin,
-                                      int indexMax);
-    virtual status_t setStreamVolumeIndex(audio_stream_type_t stream,
-                                          int index,
-                                          audio_devices_t device);
-    virtual status_t getStreamVolumeIndex(audio_stream_type_t stream,
-                                          int *index,
-                                          audio_devices_t device);
+    virtual status_t initVolumeForAttributes(
+            const audio_attributes_t &attr, int indexMin, int indexMax);
 
     virtual status_t setVolumeIndexForAttributes(const audio_attributes_t &attr,
                                                  int index,
