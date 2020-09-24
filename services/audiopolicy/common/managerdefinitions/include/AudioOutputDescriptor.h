faedbfe010816cda966f5efe3866391dddb449d5
Francois Gaffie <francois.gaffie@renault.com>
2020-09-24 17:55:08 +0200

    [AudioFlinger] Control volume using Port ID
    This CL migrates the volume management within AudioFlinger
    from stream type to port ID.
    
    It gives full power to AudioPolicy to compute the list of port
    (so MmapThreads/Tracks) on which volume control is required.
    
    It prevents from overwritting MUSIC stream type which is
    the default for volume groups without associated stream type.
    
    Test: build & play audio
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: I09bd24c9a03633b5df939544e857be4409b33623

--- services/audiopolicy/common/managerdefinitions/include/AudioOutputDescriptor.h
+++ services/audiopolicy/common/managerdefinitions/include/AudioOutputDescriptor.h
@@ -159,7 +159,7 @@ public:
     virtual uint32_t latency() { return 0; }
     virtual bool isFixedVolume(const DeviceTypeSet& deviceTypes);
     virtual bool setVolume(float volumeDb,
-                           VolumeSource volumeSource, const StreamTypeVector &streams,
+                           VolumeSource volumeSource,
                            const DeviceTypeSet& deviceTypes,
                            uint32_t delayMs,
                            bool force);
@@ -350,7 +350,7 @@ public:
         }
     }
     virtual bool setVolume(float volumeDb,
-                           VolumeSource volumeSource, const StreamTypeVector &streams,
+                           VolumeSource volumeSource,
                            const DeviceTypeSet& device,
                            uint32_t delayMs,
                            bool force);
@@ -403,6 +403,13 @@ public:
      */
     DeviceVector filterSupportedDevices(const DeviceVector &devices) const;
 
+    /**
+     * @brief getPortsForVolumeSource finds all ports matching the given volume source.
+     * @param vs to be considered
+     * @return vector of ports following the given volume source.
+     */
+    std::vector<audio_port_handle_t> getPortsForVolumeSource(const VolumeSource& vs);
+
     const sp<IOProfile> mProfile;          // I/O profile this output derives from
     audio_io_handle_t mIoHandle;           // output handle
     uint32_t mLatency;                  //
@@ -424,7 +431,7 @@ public:
             void dump(String8 *dst) const override;
 
     virtual bool setVolume(float volumeDb,
-                           VolumeSource volumeSource, const StreamTypeVector &streams,
+                           VolumeSource volumeSource,
                            const DeviceTypeSet& deviceTypes,
                            uint32_t delayMs,
                            bool force);
