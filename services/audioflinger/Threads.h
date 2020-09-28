9ae45fd57780b4138c6b4d69a101ae659577821f
Francois Gaffie <francois.gaffie@renault.com>
2020-09-28 15:30:44 +0200

    AudioFlinger: get rid of stream type for thread/track
    Bug:
    
    Test:
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: Ib9d0feb7665612c16cc9e1763750a9baaa0592b7

--- services/audioflinger/Threads.h
+++ services/audioflinger/Threads.h
@@ -1859,8 +1859,6 @@ class MmapThread : public ThreadBase
     virtual     void        processVolume_l() {}
                 void        checkInvalidTracks_l();
 
-    virtual     audio_stream_type_t streamType() { return AUDIO_STREAM_DEFAULT; }
-
     /**
      * @brief invalidateTracks invalidates all tracks identified by their port ids.
      * @param ports to be considered
@@ -1926,7 +1924,6 @@ public:
 
                 void        invalidateTracks(std::vector<audio_port_handle_t> &ports) override;
 
-    virtual     audio_stream_type_t streamType() { return mStreamType; }
     virtual     void        checkSilentMode_l();
                 void        processVolume_l() override;
 
@@ -1937,7 +1934,6 @@ public:
 protected:
                 void        dumpInternals_l(int fd, const Vector<String16>& args) override;
 
-                audio_stream_type_t         mStreamType;
                 float                       mMasterVolume;
                 float                       mPortVolume = 1.0f;
                 bool                        mMasterMute;
