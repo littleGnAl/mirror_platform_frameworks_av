bd64ae87e27e7851fd21cb09d0601dd04bfd06ba
Francois Gaffie <francois.gaffie@renault.com>
2020-09-28 15:16:02 +0200

    AudioEffect: use attributes to identify routing
    Instead of using strategy, which belong to audio policy domain,
    this CL migrates audio effect to audio attributes in order
    to allow APM/AF the audio routing followed by an effect/effect chain.
    
    Test:
    
    Bug:
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: Id5807d66db7897d63456f7da19a8dfa9c0f9888d

--- services/audiopolicy/AudioPolicyInterface.h
+++ services/audiopolicy/AudioPolicyInterface.h
@@ -204,7 +204,7 @@ public:
     virtual audio_io_handle_t getOutputForEffect(const effect_descriptor_t *desc) = 0;
     virtual status_t registerEffect(const effect_descriptor_t *desc,
                                     audio_io_handle_t io,
-                                    uint32_t strategy,
+                                    const audio_attributes_t &attributes,
                                     int session,
                                     int id) = 0;
     virtual status_t unregisterEffect(int id) = 0;
