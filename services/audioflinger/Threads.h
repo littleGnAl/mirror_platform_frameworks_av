dd0099f4cc4867fce3b4eeaa8156932ad6d0f389
Francois Gaffie <francois.gaffie@renault.com>
2020-10-19 16:12:08 +0200

    audiopolicy/audioflinger: remove stream type on getOutput
    Test: build & boot & basic audio tests
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: I24979429bd829d540580b3dde3867e58b71c211a

--- services/audioflinger/Threads.h
+++ services/audioflinger/Threads.h
@@ -859,7 +859,6 @@ public:
 
                 sp<Track>   createTrack_l(
                                 const sp<AudioFlinger::Client>& client,
-                                audio_stream_type_t streamType,
                                 const audio_attributes_t& attr,
                                 uint32_t *sampleRate,
                                 audio_format_t format,
@@ -1814,7 +1813,6 @@ class MmapThread : public ThreadBase
     virtual     ~MmapThread();
 
     virtual     void        configure(const audio_attributes_t *attr,
-                                      audio_stream_type_t streamType,
                                       audio_session_t sessionId,
                                       const sp<MmapStreamCallback>& callback,
                                       audio_port_handle_t deviceId,
@@ -1919,7 +1917,6 @@ public:
     virtual     ~MmapPlaybackThread() {}
 
     virtual     void        configure(const audio_attributes_t *attr,
-                                      audio_stream_type_t streamType,
                                       audio_session_t sessionId,
                                       const sp<MmapStreamCallback>& callback,
                                       audio_port_handle_t deviceId,
