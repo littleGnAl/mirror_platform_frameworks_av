50ffa04f05baac81926dd4baed23ac28a87a77ff
Ruchika Ashtankar <ruchika.ashtankar@ittiam.com>
2020-11-03 12:31:03 +0530

    C2Performance: Change C2MemoryUsage flag to GRALLOC_USAGE_SW_WRITE_RARELY
    Changing this flag reduces cache cleanup time, but increses the delay between process calls and fps falls.
    
    Test: stagefright -N c2.android.vp9.decoder basketball_4mbps_vp9_1x1.webm
    
    Change-Id: I1d8706466d9ad7e1853d637f5ea2c3e4d27a24e6

--- media/codec2/vndk/C2AllocatorGralloc.cpp
+++ media/codec2/vndk/C2AllocatorGralloc.cpp
@@ -60,7 +60,7 @@ C2MemoryUsage C2AndroidMemoryUsage::FromGrallocUsage(uint64_t usage) {
 uint64_t C2AndroidMemoryUsage::asGrallocUsage() const {
     // gralloc does not support WRITE_PROTECTED
     return (((expected & C2MemoryUsage::CPU_READ) ? GRALLOC_USAGE_SW_READ_OFTEN : 0) |
-            ((expected & C2MemoryUsage::CPU_WRITE) ? GRALLOC_USAGE_SW_WRITE_OFTEN : 0) |
+            ((expected & C2MemoryUsage::CPU_WRITE) ? GRALLOC_USAGE_SW_WRITE_RARELY : 0) |
             ((expected & C2MemoryUsage::READ_PROTECTED) ? GRALLOC_USAGE_PROTECTED : 0) |
             (expected & PASSTHROUGH_USAGE_MASK));
 }
