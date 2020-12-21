d43d2fb9732fd9662cacd25fd4f1c4bfc529bc43
Sungtak Lee <taklee@google.com>
2020-12-21 04:20:03 -0800

    DO NOT SUBIT enable mem profiler for swcodec
    Change-Id: Id8e21079d4a436ab3b215ab9666531d3941d97b9

--- services/mediacodec/main_swcodecservice.cpp
+++ services/mediacodec/main_swcodecservice.cpp
@@ -37,7 +37,9 @@ int main(int argc __unused, char** argv)
 {
     LOG(INFO) << "media swcodec service starting";
     signal(SIGPIPE, SIG_IGN);
-    SetUpMinijail(kSystemSeccompPolicyPath, kVendorSeccompPolicyPath);
+    (void) kSystemSeccompPolicyPath;
+    (void) kVendorSeccompPolicyPath;
+    // SetUpMinijail(kSystemSeccompPolicyPath, kVendorSeccompPolicyPath);
     strcpy(argv[0], "media.swcodec");
 
     ::android::hardware::configureRpcThreadpool(64, false);
