8479f75a1c38a7d1e294d219387f59a0fd1074ad
Francois Gaffie <francois.gaffie@renault.com>
2021-01-26 15:30:14 +0100

    audioflinger/audiopolicy: use portId to invalidateTracks
    Test: adb shell /data/nativetest64/AudioPolicyEmulatorTests/AudioPolicyEmulatorTests --gtest_filter=*InvalidateTracksTest*
    
    Change-Id: I9269ab17cc90677621a2378d6f82a55a97febbcd
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>

--- media/libaudioclient/include/media/IAudioFlinger.h
+++ media/libaudioclient/include/media/IAudioFlinger.h
@@ -445,7 +445,7 @@ public:
                                audio_input_flags_t flags) = 0;
     virtual status_t closeInput(audio_io_handle_t input) = 0;
 
-    virtual status_t invalidateStream(audio_stream_type_t stream) = 0;
+    virtual status_t invalidatePorts(const std::vector<audio_port_handle_t> &ports) = 0;
 
     virtual status_t setVoiceVolume(float volume) = 0;
 
