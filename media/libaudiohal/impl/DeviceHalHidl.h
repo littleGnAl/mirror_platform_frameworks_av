6d689919040ebe86f9bc58003a7e95e95f941670
Francois Gaffie <francois.gaffie@renault.com>
2021-02-09 15:51:24 +0100

    libaudiohal: add support of AudioGainCallback
    Bug: 154906631
    
    Test: todo
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: I062a0008dc1c4a5a693d2a4a2f166db96fd5316d

--- media/libaudiohal/impl/DeviceHalHidl.h
+++ media/libaudiohal/impl/DeviceHalHidl.h
@@ -23,12 +23,17 @@
 
 #include "ConversionHelperHidl.h"
 
+#if MAJOR_VERSION >= 7
+using ::android::hardware::audio::CPP_VERSION::IAudioGainCallback;
+#endif
 using ::android::hardware::audio::CPP_VERSION::IDevice;
 using ::android::hardware::audio::CPP_VERSION::IPrimaryDevice;
 using ::android::hardware::Return;
 
 namespace android {
 namespace CPP_VERSION {
+
+class AudioGainCallbackCallback;
 
 class DeviceHalHidl : public DeviceHalInterface, public ConversionHelperHidl
 {
@@ -119,10 +124,17 @@ class DeviceHalHidl : public DeviceHalInterface, public ConversionHelperHidl
     status_t addDeviceEffect(audio_port_handle_t device, sp<EffectHalInterface> effect) override;
     status_t removeDeviceEffect(audio_port_handle_t device, sp<EffectHalInterface> effect) override;
 
+    status_t registerAudioGainCallback(
+            const sp<DeviceHalInterfaceAudioGainCallback> &callback) override;
+    status_t unregisterAudioGainCallback(
+            const sp<DeviceHalInterfaceAudioGainCallback> &callback) override;
+
     virtual status_t dump(int fd);
 
   private:
     friend class DevicesFactoryHalHidl;
+    friend class AudioGainCallbackCallback;
+
     sp<IDevice> mDevice;
     sp<IPrimaryDevice> mPrimaryDevice;  // Null if it's not a primary device.
 
@@ -133,6 +145,15 @@ class DeviceHalHidl : public DeviceHalInterface, public ConversionHelperHidl
     virtual ~DeviceHalHidl();
 
     template <typename HalPort> status_t getAudioPortImpl(HalPort *port);
+
+#if MAJOR_VERSION >= 7
+    static status_t audioGainConfigExtToAudioPortConfigHal(
+            const ::android::hardware::hidl_vec<::android::hardware::audio::CPP_VERSION::AudioGainConfigExt>& gains,
+            std::vector<audio_port_config>& halPortConfigs);
+
+    std::mutex mAudioGainCallbackLock;
+    sp<AudioGainCallbackCallback> mAudioGainCallback; // GUARDED_BY(mAudioGainCallback)
+#endif
 };
 
 } // namespace CPP_VERSION
