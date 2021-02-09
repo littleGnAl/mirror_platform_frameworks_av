a44d6ccff584c492c2d02075a2f2517a0084d795
Francois Gaffie <francois.gaffie@renault.com>
2021-02-09 15:51:24 +0100

    WIP: libaudiohal: add support of AudioGainCallback
    Bug: 154906631
    
    Test: todo
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: I062a0008dc1c4a5a693d2a4a2f166db96fd5316d

--- media/libaudiohal/impl/DeviceHalHidl.h
+++ media/libaudiohal/impl/DeviceHalHidl.h
@@ -116,7 +116,14 @@ class DeviceHalHidl : public DeviceHalInterface, public ConversionHelperHidl
     status_t addDeviceEffect(audio_port_handle_t device, sp<EffectHalInterface> effect) override;
     status_t removeDeviceEffect(audio_port_handle_t device, sp<EffectHalInterface> effect) override;
 
+    status_t registerAudioGainCallback(
+            const sp<DeviceHalInterfaceAudioGainCallback> &callback) override;
+    status_t unregisterAudioGainCallback(
+            const sp<DeviceHalInterfaceAudioGainCallback> &callback) override;
+
     virtual status_t dump(int fd);
+
+    wp<DeviceHalInterfaceAudioGainCallback> mDeviceHalInterfaceAudioGainCallback;
 
   private:
     friend class DevicesFactoryHalHidl;
@@ -128,6 +135,10 @@ class DeviceHalHidl : public DeviceHalInterface, public ConversionHelperHidl
 
     // The destructor automatically closes the device.
     virtual ~DeviceHalHidl();
+
+#if MAJOR_VERSION >= 7
+    sp<android::hardware::audio::audiogain::CPP_VERSION::IAudioGainCallback> mAudioGainCallback;
+#endif
 };
 
 } // namespace CPP_VERSION
