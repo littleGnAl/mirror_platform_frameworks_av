31f69b349e0fb24c80d3d3310cf29b08ade4a966
Francois Gaffie <francois.gaffie@renault.com>
2021-05-05 10:49:17 +0200

    AudioFlinger: PatchPanel: prioritize quality vs latency for FM Bridge
    When a SwBridge is created, the default behavior is to start playback as
    soon as possible, prioritizing the lowest strategy vs quality (potential
    glitches).
    
    This CL prevents this behavior for FM Tuner input devices if no fast mixer
    and fast capture are used.
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: I24cfb97d09518be4ab15775d8575957a220c25dc

--- services/audioflinger/PatchPanel.cpp
+++ services/audioflinger/PatchPanel.cpp
@@ -577,6 +577,12 @@ status_t AudioFlinger::PatchPanel::Patch::createConnections(
 
     // create a special playback track to render to playback thread.
     // this track is given the same buffer as the PatchRecord buffer
+
+    // Default behaviour is to start as soon as possible to have the lowest possible latency even if
+    // it might glitch.
+    // Disable this behavior for FM Tuner source if no fast capture/mixer available.
+    const bool isFmBridge = mAudioPatch.sources[0].ext.device.type == AUDIO_DEVICE_IN_FM_TUNER;
+    const size_t frameCountToBeReady = isFmBridge && !usePassthruPatchRecord ? frameCount / 4 : 1;
     sp<PlaybackThread::PatchTrack> tempPatchTrack = new PlaybackThread::PatchTrack(
                                            mPlayback.thread().get(),
                                            streamType,
@@ -587,7 +593,9 @@ status_t AudioFlinger::PatchPanel::Patch::createConnections(
                                            tempRecordTrack->buffer(),
                                            tempRecordTrack->bufferSize(),
                                            clientAttSource,
-                                           outputFlags);
+                                           outputFlags,
+                                           {} /*timeout*/,
+                                           frameCountToBeReady);
     status = mPlayback.checkTrack(tempPatchTrack.get());
     if (status != NO_ERROR) {
         return status;
