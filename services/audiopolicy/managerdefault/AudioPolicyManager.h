4194fdd303c913ae25e3ffd647e2db3c25f7d13c
Francois Gaffie <francois.gaffie@renault.com>
2021-06-22 13:27:39 +0200

    [BUG] AudioPolicyManager: prevent unecessary force routing on SwOutput
    At the begining/end of call, routing for all SwOutput is evaluated.
    In case of using bridge, either the output involved in the SwBridge or
    the output involved in the HwBridge for volume will be forced to be rerouted
    on same device as the routing has already been evaluated for the Swoutput
    hosting the call..
    
    This CL implements this optimization.
    
    Bug: 187173302
    
    Test: CSV & VoIP calls
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: I15102b661a4fc4afdd10888011feadf1fa7084ce

--- services/audiopolicy/managerdefault/AudioPolicyManager.h
+++ services/audiopolicy/managerdefault/AudioPolicyManager.h
@@ -575,18 +575,22 @@ protected:
         void updateCallAndOutputRouting(bool forceVolumeReeval = true, uint32_t delayMs = 0);
 
         bool isCallRxAudioSource(const sp<SourceClientDescriptor> &source) {
-            return mCallRxSourceClientPort != AUDIO_PORT_HANDLE_NONE
-                && source == mAudioSources.valueFor(mCallRxSourceClientPort);
+            return mCallRxSourceClient != nullptr && source == mCallRxSourceClient;
         }
 
         void connectTelephonyRxAudioSource();
 
-        void disconnectTelephonyAudioSource(audio_port_handle_t &portHandle);
+        void disconnectTelephonyAudioSource(sp<SourceClientDescriptor> &clientDesc);
 
         void connectTelephonyTxAudioSource(const sp<DeviceDescriptor> &srcdevice,
                                            const sp<DeviceDescriptor> &sinkDevice,
                                            uint32_t delayMs);
 
+        bool isTelephonyRxOrTx(const sp<SwAudioOutputDescriptor>& desc) const {
+            return (mCallRxSourceClient != nullptr && mCallRxSourceClient->belongsToOutput(desc))
+                    || (mCallTxSourceClient != nullptr
+                    &&  mCallTxSourceClient->belongsToOutput(desc));
+        }
 
         /**
          * @brief updates routing for all inputs.
@@ -886,8 +890,8 @@ protected:
 
         // The port handle of the hardware audio source created internally for the Call RX audio
         // end point.
-        audio_port_handle_t mCallRxSourceClientPort = AUDIO_PORT_HANDLE_NONE;
-        audio_port_handle_t mCallTxSourceClientPort = AUDIO_PORT_HANDLE_NONE;
+        sp<SourceClientDescriptor> mCallRxSourceClient;
+        sp<SourceClientDescriptor> mCallTxSourceClient;
 
         // Support for Multi-Stream Decoder (MSD) module
         sp<DeviceDescriptor> getMsdAudioInDevice() const;
@@ -910,6 +914,10 @@ protected:
         void releaseMsdOutputPatches(const DeviceVector& devices);
         bool isMsdPatch(const audio_patch_handle_t &handle) const;
 private:
+        sp<SourceClientDescriptor> startAudioSourceInternal(
+                const struct audio_port_config *source, const audio_attributes_t *attributes,
+                uid_t uid);
+
         void onNewAudioModulesAvailableInt(DeviceVector *newDevices);
 
         // Add or remove AC3 DTS encodings based on user preferences.
