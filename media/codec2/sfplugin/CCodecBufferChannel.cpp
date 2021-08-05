80eafd75ad3cd77e22ea3aeb01a5f1ef1d5dd0d6
Jiajia Cong <jcong@codeaurora.org>
2021-08-05 16:33:35 +0800

    codec2: add smoothfactor when checking n-th work
    pipeline watcher's queued work counter included smoothfactor, if we
    don't add it in elapsed(), it is easy to cause CCodec queue call timeout
    
    Change-Id: Ia2dbf3a7cf0da4805398cf44780ad3dacbaf1a1b

--- media/codec2/sfplugin/CCodecBufferChannel.cpp
+++ media/codec2/sfplugin/CCodecBufferChannel.cpp
@@ -2053,7 +2053,9 @@ PipelineWatcher::Clock::duration CCodecBufferChannel::elapsed() {
     if (!mInputMetEos) {
         size_t outputDelay = mOutput.lock()->outputDelay;
         Mutexed<Input>::Locked input(mInput);
-        n = input->inputDelay + input->pipelineDelay + outputDelay;
+        n = input->inputDelay + input->pipelineDelay + outputDelay + kSmoothnessFactor;
+        ALOGD("[%s] DEBUG: elapsed: n=%zu [in=%u pipeline=%u out=%zu]", mName, n,
+                input->inputDelay, input->pipelineDelay, outputDelay);
     }
     return mPipelineWatcher.lock()->elapsed(PipelineWatcher::Clock::now(), n);
 }
