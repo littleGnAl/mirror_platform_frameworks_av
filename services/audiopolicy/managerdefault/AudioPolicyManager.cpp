5531007d7574aecaf5c9ee9487b8590f5a72822e
Francois Gaffie <francois.gaffie@renault.com>
2021-11-17 17:04:32 +0100

    [BUG] AudioPolicyManager: fix port id alignment with JAVA layer
    CL:
    https://android-review.googlesource.com/c/platform/frameworks/av/+/1863726
    introduced a regression.
    
    A device equality is based on {type,address} couple.
    If a device is mounted, attached to a  module, an id is assigned.
    if the device is unmounted, thus detached from module, id is resetted.
    If this device is mounted again, thus attached to same module,
    the routing may not have change (aka same output device for the concerned
    SwOutput), setOutputDevice will not recreate/refresh the patch associated.
    It may lead to misalignement with JAVA listAudioPort/listAudioPatch API
    based on the mId to associated sources/sink of a patch to the port.
    
    This CL ensures the patch is either removed (output is closed)
    or updated (change of output device) depending if the
    output used to reach the concerned device is still needed or not
    after disconnection.
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: Ibfce32e2fea6dfb38afa0787ee1bddfc5fede0aa

--- services/audiopolicy/managerdefault/AudioPolicyManager.cpp
+++ services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -249,6 +249,19 @@ status_t AudioPolicyManager::setDeviceConnectionStateInt(const sp<DeviceDescript
                     if ((state == AUDIO_POLICY_DEVICE_STATE_UNAVAILABLE) ||
                             (((desc->mFlags & AUDIO_OUTPUT_FLAG_DIRECT) != 0) &&
                                 (desc->mDirectOpenCount == 0))) {
+                        if (desc == mPrimaryOutput) {
+                            // Before closing primary, if ensure this output may not be used to
+                            // reach still available device.
+                            // Otherwise, the primary output will not be assigned anymore until
+                            // a device supported by primary output is (re)connected
+                            const auto newDevices = desc->isActive() ?
+                                        getNewOutputDevices(desc, false /*fromCache*/) :
+                                        desc->supportedDevices();
+                            if (!mAvailableOutputDevices.filter(newDevices).empty()) {
+                                setOutputDevices(desc, newDevices, false/*force*/, 0);
+                                continue;
+                            }
+                        }
                         clearAudioSourcesForOutput(output);
                         closeOutput(output);
                     }
@@ -5218,8 +5231,9 @@ status_t AudioPolicyManager::checkOutputsForDevice(const sp<DeviceDescriptor>& d
             if (!desc->isDuplicated()) {
                 // exact match on device
                 if (device_distinguishes_on_address(deviceType) && desc->supportsDevice(device)
-                        && desc->containsSingleDeviceSupportingEncodedFormats(device)
-                        && !mAvailableOutputDevices.containsAtLeastOne(desc->supportedDevices())) {
+                        && desc->containsSingleDeviceSupportingEncodedFormats(device)) {
+                    ALOGV("checkOutputsForDevice(): disconnecting device %s adding output %d",
+                            device->toString().c_str(), mOutputs.keyAt(i));
                     outputs.add(mOutputs.keyAt(i));
                 } else if (!mAvailableOutputDevices.containsAtLeastOne(desc->supportedDevices())) {
                     ALOGV("checkOutputsForDevice(): disconnecting adding output %d",
