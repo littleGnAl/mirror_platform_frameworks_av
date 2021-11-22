2b9b3b213ede31d3275a67801f7fa556a2fb0442
Francois Gaffie <francois.gaffie@renault.com>
2021-11-22 18:20:16 +0100

    [BUG] Keep track of music effect thread when primary output is unavailable
    Audio Policy Manager may temporary run without primary output.
    If music effects are instantiated, APM will loose track of music
    output (may point on unvalid io handle of primary output).
    AF will save these effect on first playback thread.
    Thus, these music effects become impossible to be moved back to
    music playback thread.
    
    This CL fixes this by:
        -using chain parking mecanism in audio flinger when output
    hosting session mix is closed
        -fixing session mix creation if APM has temporarily no output,
    in this case, AudioFlinger will use first output available to host
    output mix effect
        -enhancing moveEffect function to check in the orphan chains if
    the source output is not found
        -enhancing moveEffect function to check amoung other output
    when source is not found to find the output mix session.
    
    Signed-off-by: Francois Gaffie <francois.gaffie@renault.com>
    Change-Id: I7690aab62d6dabc6351e7b283810bdcaf92bb3de

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -2714,6 +2714,14 @@ status_t AudioFlinger::closeOutput_nonvirtual(audio_io_handle_t output)
 
 
             mPlaybackThreads.removeItem(output);
+            // Save AUDIO_SESSION_OUTPUT_MIX effect to chain orphans
+            if (playbackThread->hasAudioSession(AUDIO_SESSION_OUTPUT_MIX)) {
+                Mutex::Autolock _sppl(playbackThread->mLock);
+                auto mixChain = playbackThread->getEffectChain_l(AUDIO_SESSION_OUTPUT_MIX);
+                ALOGV("%s() output %d moving mix session to orphans", __func__, output);
+                putOrphanEffectChain_l(mixChain);
+                playbackThread->removeEffectChain_l(mixChain);
+            }
             // save all effects to the default thread
             if (mPlaybackThreads.size()) {
                 PlaybackThread *dstThread = checkPlaybackThread_l(mPlaybackThreads.keyAt(0));
@@ -3780,7 +3788,8 @@ status_t AudioFlinger::createEffect(const media::CreateEffectRequest& request,
             // before creating the AudioEffect or the io handle must be specified.
             //
             // Detect if the effect is created after an AudioRecord is destroyed.
-            if (getOrphanEffectChain_l(sessionId).get() != nullptr) {
+            if ((sessionId != AUDIO_SESSION_OUTPUT_MIX) &&
+                    getOrphanEffectChain_l(sessionId).get() != nullptr) {
                 ALOGE("%s: effect %s with no specified io handle is denied because the AudioRecord"
                       " for session %d no longer exists",
                       __func__, descOut.name, sessionId);
@@ -3825,6 +3834,7 @@ status_t AudioFlinger::createEffect(const media::CreateEffectRequest& request,
             }
         }
         ThreadBase *thread = checkRecordThread_l(io);
+        const bool isPreProcessing = (thread != nullptr);
         if (thread == NULL) {
             thread = checkPlaybackThread_l(io);
             if (thread == NULL) {
@@ -3835,7 +3845,8 @@ status_t AudioFlinger::createEffect(const media::CreateEffectRequest& request,
                     goto Exit;
                 }
             }
-        } else {
+        }
+        if (isPreProcessing || sessionId == AUDIO_SESSION_OUTPUT_MIX) {
             // Check if one effect chain was awaiting for an effect to be created on this
             // session and used it instead of creating a new one.
             sp<EffectChain> chain = getOrphanEffectChain_l(sessionId);
@@ -3915,7 +3926,19 @@ status_t AudioFlinger::moveEffects(audio_session_t sessionId, audio_io_handle_t 
         return NO_ERROR;
     }
     PlaybackThread *srcThread = checkPlaybackThread_l(srcOutput);
-    if (srcThread == NULL) {
+    const bool foundOrphanForSessionId = (mOrphanEffectChains.indexOfKey(sessionId) >= 0);
+    if (srcThread == NULL && !foundOrphanForSessionId && sessionId == AUDIO_SESSION_OUTPUT_MIX) {
+        ALOGW("%s() session %d not found in orphans, checking other mix", __func__, sessionId);
+        for (size_t i = 0; i < mPlaybackThreads.size(); i++) {
+            if (mPlaybackThreads.valueAt(i)->hasAudioSession(sessionId)) {
+                srcThread = mPlaybackThreads.valueAt(i).get();
+                ALOGW("%s() found srcOutput %d hosting session %d", __func__,
+                      mPlaybackThreads.keyAt(i), sessionId);
+                break;
+            }
+        }
+    }
+    if (srcThread == NULL && !foundOrphanForSessionId) {
         ALOGW("moveEffects() bad srcOutput %d", srcOutput);
         return BAD_VALUE;
     }
@@ -3926,6 +3949,11 @@ status_t AudioFlinger::moveEffects(audio_session_t sessionId, audio_io_handle_t 
     }
 
     Mutex::Autolock _dl(dstThread->mLock);
+    if (foundOrphanForSessionId) {
+        ALOGW("moveEffects() found session %d in orphan chains", sessionId);
+        sp<EffectChain> chain = getOrphanEffectChain_l(sessionId);
+        return dstThread->addEffectChain_l(chain);
+    }
     Mutex::Autolock _sl(srcThread->mLock);
     return moveEffectChain_l(sessionId, srcThread, dstThread);
 }
