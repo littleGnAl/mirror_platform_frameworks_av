046148788c6763fd1a287629b78e98db3957d08c
ziyangch <ziyangch@google.com>
2021-12-01 13:48:04 -0800

    Prevent offload underrun during active playback.
    Bug: 177944032
    Test: Start playback in Youtube app.
    Merged-In: Ib60b7d049e159d0f32daa5e586ff92dc0bfd48ee
    Change-Id: Ib60b7d049e159d0f32daa5e586ff92dc0bfd48ee
    (cherry picked from commit 8f194f194d8520896349eef110f39f614fd23b88)
    (cherry picked from commit 09204bd915b597d608fa2e3670d30aa5c0ccd274)

--- services/audioflinger/Threads.h
+++ services/audioflinger/Threads.h
@@ -1217,6 +1217,21 @@ protected:
                 // volumes last sent to audio HAL with stream->setVolume()
                 float mLeftVolFloat;
                 float mRightVolFloat;
+<<<<<<< HEAD   (87bcb2 Merge "VTS: Disable codec2 tests if platform does not suppor)
+=======
+
+                // audio patch used by the downstream software patch.
+                // Only used if ThreadBase::mIsMsdDevice is true.
+                struct audio_patch mDownStreamPatch;
+
+                // A differential check on the timestamps to see if there is a change in the
+                // timestamp frame position between the last call to checkRunningTimestamp.
+                uint64_t mLastCheckedTimestampPosition = ~0LL;
+
+                bool checkRunningTimestamp();
+
+    virtual     void flushHw_l() { mLastCheckedTimestampPosition = ~0LL; }
+>>>>>>> CHANGE (09204b Prevent offload underrun during active playback.)
 };
 
 class MixerThread : public PlaybackThread {
@@ -1333,7 +1348,7 @@ public:
     virtual     bool        checkForNewParameter_l(const String8& keyValuePair,
                                                    status_t& status);
 
-    virtual     void        flushHw_l();
+                void        flushHw_l() override;
 
                 void        setMasterBalance(float balance) override;
 
@@ -1398,7 +1413,7 @@ public:
     OffloadThread(const sp<AudioFlinger>& audioFlinger, AudioStreamOut* output,
                   audio_io_handle_t id, bool systemReady);
     virtual                 ~OffloadThread() {};
-    virtual     void        flushHw_l();
+                void        flushHw_l() override;
 
 protected:
     // threadLoop snippets
@@ -1415,10 +1430,6 @@ private:
     size_t      mPausedWriteLength;     // length in bytes of write interrupted by pause
     size_t      mPausedBytesRemaining;  // bytes still waiting in mixbuffer after resume
     bool        mKeepWakeLock;          // keep wake lock while waiting for write callback
-    uint64_t    mOffloadUnderrunPosition; // Current frame position for offloaded playback
-                                          // used and valid only during underrun.  ~0 if
-                                          // no underrun has occurred during playback and
-                                          // is not reset on standby.
 };
 
 class AsyncCallbackThread : public Thread {
