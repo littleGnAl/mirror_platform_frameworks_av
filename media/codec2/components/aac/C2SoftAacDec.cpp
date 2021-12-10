9d62cdae551de29b92d86ff9ab7afbfaaa636e19
Liang Ji <liang.ji@amlogic.com>
2021-12-10 21:28:19 +0800

    Audio: CtsMediaTestCases:ClearKeyPlayback fail [1/1]
    PD#SWPL-66387
    Bug: 274889277
    
    Problem:
    audio pts is far away media time, lead to video cann't render;
    only audio pts updated, and close to media time, then video could
    render;
    but aac decoder output buffer is big, only it has enough pcm data, then
    decoder could output them;
    so aac decoder need more es data to decode;
    but aac decoder cann't obtain es because video es hasn't been sent to
    video decoder(av use same extrator);
    and only after video rendered, video es could be decoded again.
    so above said is a loop, and cause "dead lock" when "error" happens.
    
    Solution:
    let aac decoder output more pcm data,
    then audio pts would be close to media time,
    then video could render normally.
    
    Verify:
    ohm.
    
    Change-Id: I6a46818cedfb1b6d0411738978a63ca31d5a5106
    Signed-off-by: wei.wang1 <wei.wang1@amlogic.com>

--- media/codec2/components/aac/C2SoftAacDec.cpp
+++ media/codec2/components/aac/C2SoftAacDec.cpp
@@ -488,6 +488,7 @@ void C2SoftAacDec::drainRingBuffer(
         int available = outputDelayRingBufferSamplesAvailable();
         int numFrames = outInfo.decodedSizes.size();
         int numSamples = numFrames * (mStreamInfo->frameSize * mStreamInfo->numChannels);
+
         if (available < numSamples) {
             if (eos) {
                 numSamples = available;
@@ -495,6 +496,9 @@ void C2SoftAacDec::drainRingBuffer(
                 break;
             }
         }
+        else
+            numSamples = available;
+
         ALOGV("%d samples available (%d), or %d frames",
                 numSamples, available, numFrames);
         ALOGV("getting %d from ringbuffer", numSamples);
