1aac9b4e74d8616913df5d9ec8f922f17237e187
Yushin Cho <yushin@google.com>
2021-12-09 16:55:21 -0800

    Add corresponding PictureType constants defined in MediaFormat to NDK
    Bug: 205793764
    Bug: 203696003
    Test: m all
    
    Change-Id: I4835d52f53ece3911669a7b61087af24b3c38965

--- media/libstagefright/include/media/stagefright/MediaCodecConstants.h
+++ media/libstagefright/include/media/stagefright/MediaCodecConstants.h
@@ -738,6 +738,11 @@ constexpr int32_t COLOR_TRANSFER_LINEAR = 1;
 constexpr int32_t COLOR_TRANSFER_SDR_VIDEO = 3;
 constexpr int32_t COLOR_TRANSFER_ST2084 = 6;
 
+constexpr int32_t PICTURE_TYPE_NONE = 0;
+constexpr int32_t PICTURE_TYPE_I = 1;
+constexpr int32_t PICTURE_TYPE_P = 2;
+constexpr int32_t PICTURE_TYPE_B = 3;
+
 constexpr char KEY_AAC_DRC_ALBUM_MODE[] = "aac-drc-album-mode";
 constexpr char KEY_AAC_DRC_ATTENUATION_FACTOR[] = "aac-drc-cut-level";
 constexpr char KEY_AAC_DRC_BOOST_FACTOR[] = "aac-drc-boost-level";
