54bc0db14cb6ae6aad010661e8a66fd73eb7ce6a
Lubin Yin <quic_lubiny@quicinc.com>
2022-02-24 14:20:17 -0800

    codec2: Add android fence implementation for C2Fence
    Add a new C2Fence implementation that wraps a android fence, which is
    based on linux sync file and can be transported through IPC.
    
    Change-Id: I14f66e6f4cf4ff3c878a3d8e78988c50d72c9a21

--- media/codec2/sfplugin/CCodecBufferChannel.cpp
+++ media/codec2/sfplugin/CCodecBufferChannel.cpp
@@ -875,6 +875,17 @@ status_t CCodecBufferChannel::renderOutputBuffer(
         return UNKNOWN_ERROR;
     }
     const C2ConstGraphicBlock &block = blocks.front();
+    C2Fence c2fence = block.fence();
+    sp<Fence> fence = (c2fence.isHW() ? sp<Fence>::make(c2fence.fd()) : Fence::NO_FENCE);
+    if (c2fence.isHW()) {
+        int fenceFd = c2fence.fd();
+        fence = sp<Fence>::make(fenceFd);
+        if (!fence) {
+            ALOGE("[%s] Failed to allocate a fence", mName);
+            close(fenceFd);
+            return NO_MEMORY;
+        }
+    }
 
     // TODO: revisit this after C2Fence implementation.
     android::IGraphicBufferProducer::QueueBufferInput qbi(
@@ -887,7 +898,7 @@ status_t CCodecBufferChannel::renderOutputBuffer(
                  blocks.front().crop().bottom()),
             videoScalingMode,
             transform,
-            Fence::NO_FENCE, 0);
+            fence, 0);
     if (hdrStaticInfo || hdrDynamicInfo) {
         HdrMetadata hdr;
         if (hdrStaticInfo) {
