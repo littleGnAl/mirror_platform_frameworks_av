a8991bbd1eb9bf122d738a4ece020e922fc0575a
Houxiang Dai <houxiang.dai@mediatek.com>
2022-03-22 19:17:51 +0800

    output format update for tunnel&non-block mode
    Some APPs want to getOutputFormat in tunnel&non-block mode.
    Framework add support update output format changed notify to
    MediaCodec for this scene.
    
    Test:
    EXO getOutputFormat success when resolution changed.
    
    Bug: 225293223
    Change-Id: I01963acf204c696835f0bc32e5cb413c6c0b9416

--- media/libstagefright/MediaCodec.cpp
+++ media/libstagefright/MediaCodec.cpp
@@ -3536,12 +3536,8 @@ void MediaCodec::onMessageReceived(const sp<AMessage> &msg) {
                         sp<RefBase> obj;
                         CHECK(msg->findObject("buffer", &obj));
                         sp<MediaCodecBuffer> buffer = static_cast<MediaCodecBuffer *>(obj.get());
-                        if (mFlags & kFlagIsAsync) {
-                            // In asynchronous mode, output format change is processed immediately.
-                            handleOutputFormatChangeIfNeeded(buffer);
-                        } else {
-                            postActivityNotificationIfPossible();
-                        }
+                        // Output format change is processed immediately for tunneled without BlockModel
+                        handleOutputFormatChangeIfNeeded(buffer);
                         mBufferChannel->discardBuffer(buffer);
                         break;
                     }
