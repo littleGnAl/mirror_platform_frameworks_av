26c9109561944618ad10d4d367e70606a1a39587
Harish Mahendrakar <harish.mahendrakar@ittiam.com>
2022-04-13 15:53:55 -0700

    Use AHardwareBuffer_isSupported() instead of an extra allocation
    AHardwareBuffer_isSupported() now supports P010 format and thus
    can be used instead of an extra allocation that was done earlier to
    check if a format was supported.
    
    Bug: 214411172
    Test: atest CtsMediaDecoderTestCases:ImageReaderDecoderTest -- \
     --module-arg \
     CtsMediaDecoderTestCases:instrumentation-arg:codec-prefix:=c2.android.
    Test: atest android.mediav2.cts.CodecDecoderSurfaceTest \
     android.mediav2.cts.AdaptivePlaybackTest \
     android.mediav2.cts.CodecDecoderTest -- --module-arg \
     CtsMediaV2TestCases:instrumentation-arg:codec-prefix:=c2.android.
    
    Change-Id: Iee516a1d45ab82308e74fe6941be748a12bab8af

--- media/codec2/components/base/SimpleC2Component.cpp
+++ media/codec2/components/base/SimpleC2Component.cpp
@@ -18,6 +18,7 @@
 #define LOG_TAG "SimpleC2Component"
 #include <log/log.h>
 
+#include <android/hardware_buffer.h>
 #include <cutils/properties.h>
 #include <media/stagefright/foundation/AMessage.h>
 
@@ -26,6 +27,7 @@
 #include <C2Config.h>
 #include <C2Debug.h>
 #include <C2PlatformSupport.h>
+#include <Codec2BufferUtils.h>
 #include <SimpleC2Component.h>
 
 namespace android {
@@ -893,17 +895,7 @@ int SimpleC2Component::getHalPixelFormatForBitDepth10(bool allowRGBA1010102) {
         halPixelFormats.push_back(HAL_PIXEL_FORMAT_RGBA_1010102);
 
         for (int halPixelFormat : halPixelFormats) {
-            std::shared_ptr<C2GraphicBlock> block;
-
-            uint32_t gpuConsumerFlags = halPixelFormat == HAL_PIXEL_FORMAT_RGBA_1010102
-                                                ? C2AndroidMemoryUsage::HW_TEXTURE_READ
-                                                : 0;
-            C2MemoryUsage usage = {C2MemoryUsage::CPU_READ | gpuConsumerFlags,
-                                   C2MemoryUsage::CPU_WRITE};
-            // TODO(b/214411172) Use AHardwareBuffer_isSupported once it supports P010
-            c2_status_t status =
-                    mOutputBlockPool->fetchGraphicBlock(320, 240, halPixelFormat, usage, &block);
-            if (status == C2_OK) {
+            if (isHalPixelFormatSupported((AHardwareBuffer_Format)halPixelFormat)) {
                 mBitDepth10HalPixelFormats.push_back(halPixelFormat);
             }
         }
