d0bf059773ecf0514dfd30cdebd9ccfaab7f5784
Harish Mahendrakar <harish.mahendrakar@ittiam.com>
2022-04-13 15:53:55 -0700

    Use AHardwareBuffer_isSupported() instead of an extra allocation
    AHardwareBuffer_isSupported() now supports P010 format and thus
    can be used instead of an extra allocation that was done earlier to
    check if a format was supported.
    
    Bug: 214411172
    Test: Enabled P010 in c2 av1 and vp9 locally and tested the following
    Test: atest CtsMediaDecoderTestCases:ImageReaderDecoderTest -- \
     --module-arg \
     CtsMediaDecoderTestCases:instrumentation-arg:codec-prefix:=c2.android.
    Test: atest android.mediav2.cts.CodecDecoderSurfaceTest \
     android.mediav2.cts.AdaptivePlaybackTest \
     android.mediav2.cts.CodecDecoderTest -- --module-arg \
     CtsMediaV2TestCases:instrumentation-arg:codec-prefix:=c2.android.
    
    Change-Id: Iee516a1d45ab82308e74fe6941be748a12bab8af

--- media/codec2/components/gav1/C2SoftGav1Dec.cpp
+++ media/codec2/components/gav1/C2SoftGav1Dec.cpp
@@ -190,8 +190,8 @@ class C2SoftGav1Dec::IntfImpl : public SimpleInterface<void>::BaseParams {
               .build());
 
     std::vector<uint32_t> pixelFormats = {HAL_PIXEL_FORMAT_YCBCR_420_888};
-    // TODO(b/229387180) Enable once P010 is fully supported
-    if (false && isAtLeastT()) {
+    if (isAtLeastT() &&
+        isHalPixelFormatSupported((AHardwareBuffer_Format)HAL_PIXEL_FORMAT_YCBCR_P010)) {
         pixelFormats.push_back(HAL_PIXEL_FORMAT_YCBCR_P010);
     }
     // TODO: support more formats?
