297409fb05766908148848e0b8edb766e5e8a217
Malathi Gottam <quic_mgottam@quicinc.com>
2022-07-12 18:28:32 +0530

    sfplugin: do not fail resume call if inputs slots are active
    On signalling resume, initial input buffers are requested, but
    there is a chance that i/p buffers might already be available
    to client or with component, skip resume call failure in such
    scenarios.
    
    Bug-ID: 288516852
    Change-Id: Ic23d1b26da900c6089905747e5f5364ed0482fb4

--- media/codec2/sfplugin/CCodec.cpp
+++ media/codec2/sfplugin/CCodec.cpp
@@ -2140,9 +2140,11 @@ void CCodec::signalResume() {
 
     std::map<size_t, sp<MediaCodecBuffer>> clientInputBuffers;
     status_t err = mChannel->prepareInitialInputBuffers(&clientInputBuffers);
-    // FIXME(b/237656746)
-    if (err != OK && err != NO_MEMORY) {
-        ALOGE("Resume request for Input Buffers failed");
+    if (err != OK) {
+        if (err == WOULD_BLOCK) {
+            return;
+        }
+        ALOGW("Resume request for Input Buffers failed");
         mCallback->onError(err, ACTION_CODE_FATAL);
         return;
     }
