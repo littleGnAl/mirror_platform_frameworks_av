2aa81a98b77e30263a35548660cd12c7e4165ced
Henri Chataing <henrichataing@google.com>
2022-08-31 18:31:28 +0000

    Revert "[HWAsan Test]fix use-after-free bug"
    This reverts commit 0e1f56e2806875e130c719e46888523ac503726a.
    
    Reason for revert: DroidMonitor-triggered revert due to breakage https://android-build.googleplex.com/builds/tests/view?invocationId=I27300010086294446&testResultId=TR01728236948982953, bug http://b/244453224
    
    Change-Id: Ic84061ffb33ef79f79215767433679e0ad409cef

--- services/audiopolicy/service/AudioPolicyService.cpp
+++ services/audiopolicy/service/AudioPolicyService.cpp
@@ -1721,7 +1721,6 @@ void AudioPolicyService::UidPolicy::updateUidLocked(std::unordered_map<uid_t,
 }
 
 bool AudioPolicyService::UidPolicy::isA11yOnTop() {
-    Mutex::Autolock _l(mLock);
     for (const auto &uid : mCachedUids) {
         if (!isA11yUid(uid.first)) {
             continue;
