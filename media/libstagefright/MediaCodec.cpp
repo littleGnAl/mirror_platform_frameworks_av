94d9f14d01987b0f595c13511d1539be7faf381e
Jason Macnak <natsu@google.com>
2022-09-08 14:53:21 -0700

    DO NOT SUBMIT: Send addResource() calls for software codecs too
    Bug: b/201748251
    Test: cts -m ResourceObserverNativeTest
    Change-Id: I705e834a941cc09a698eae3f5af40f714d3bd7b3

--- media/libstagefright/MediaCodec.cpp
+++ media/libstagefright/MediaCodec.cpp
@@ -3185,14 +3185,14 @@ void MediaCodec::onMessageReceived(const sp<AMessage> &msg) {
                         mediametrics_setInt32(mMetricsHandle, kCodecSecure, 0);
                     }
 
-                    MediaCodecInfo::Attributes attr = mCodecInfo
-                            ? mCodecInfo->getAttributes()
-                            : MediaCodecInfo::Attributes(0);
-                    if (!(attr & MediaCodecInfo::kFlagIsSoftwareOnly)) {
+                    //MediaCodecInfo::Attributes attr = mCodecInfo
+                    //        ? mCodecInfo->getAttributes()
+                    //        : MediaCodecInfo::Attributes(0);
+                    //if (!(attr & MediaCodecInfo::kFlagIsSoftwareOnly)) {
                         // software codec is currently ignored.
                         mResourceManagerProxy->addResource(
                                 MediaResource::CodecResource(mFlags & kFlagIsSecure, mIsVideo));
-                    }
+                    //}
 
                     postPendingRepliesAndDeferredMessages("kWhatComponentAllocated");
                     break;
