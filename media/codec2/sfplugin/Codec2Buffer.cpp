f1f4c1fa461e943b27657568107746218ffc9218
Wonsik Kim <wonsik@google.com>
2022-09-20 11:00:42 -0700

    CCodec: fix wrapping logic
    C2PlaneInfo::maxOffset takes care of the allocated bit depth, so
    adding for the last byte is not needed.
    
    Bug: 246678061
    Test: presubmit
    Change-Id: I656d8e8555edae602ed8e344f0ce16f13f2fb858

--- media/codec2/sfplugin/Codec2Buffer.cpp
+++ media/codec2/sfplugin/Codec2Buffer.cpp
@@ -533,7 +533,7 @@ public:
                         * align(mHeight, 64) / plane.rowSampling;
             }
 
-            if (minPtr == mView.data()[0] && (maxPtr - minPtr + 1) <= planeSize) {
+            if (minPtr == mView.data()[0] && (maxPtr - minPtr) <= planeSize) {
                 // FIXME: this is risky as reading/writing data out of bound results
                 //        in an undefined behavior, but gralloc does assume a
                 //        contiguous mapping
