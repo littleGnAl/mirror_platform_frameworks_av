98e66e051748286578cc2873a5d15ae7f61668a6
luming1 <luming1@xiaomi.com>
2022-10-29 12:55:58 +0800

    reset device codec format after checkForDeviceAndOutputChanges
    test:
    1. connecting remote-submix and a2dp.
    2. playing music using deep buffer output.
    2. disconnect a2dp.
    
    expect: music restores track and output select to duplicate.
    actual: music didn't restore the track still using deep buffer
            output and changing the ad2p device to none.
    
    root cause: old device a2dp reset codec format to default
    before active output selection. Will not get active output
    because the device encoder format was not supported after reset.
    
    Change-Id: I3249f43c145e07a6335e658aa25f17840e014c50
    Signed-off-by: luming1 <luming1@xiaomi.com>

--- services/audiopolicy/managerdefault/AudioPolicyManager.cpp
+++ services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -254,9 +254,6 @@ status_t AudioPolicyManager::setDeviceConnectionStateInt(const sp<DeviceDescript
 
             checkOutputsForDevice(device, state, outputs);
 
-            // Reset active device codec
-            device->setEncodedFormat(AUDIO_FORMAT_DEFAULT);
-
             // remove device from mReportedFormatsMap cache
             mReportedFormatsMap.erase(device);
 
@@ -360,6 +357,8 @@ status_t AudioPolicyManager::setDeviceConnectionStateInt(const sp<DeviceDescript
         }
 
         if (state == AUDIO_POLICY_DEVICE_STATE_UNAVAILABLE) {
+            // Reset active device codec
+            device->setEncodedFormat(AUDIO_FORMAT_DEFAULT);
             cleanUpForDevice(device);
         }
 
