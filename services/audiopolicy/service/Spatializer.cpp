d7b93d0e5cdca3185ffb43564b0f93e449e84c70
Faye Yan <fayey@google.com>
2022-12-29 18:30:29 +0000

    Revert "Spatializer: Log latency requests to/from output sink"
    This reverts commit 5d8618db1211d8f59376a59d53d4f07c599bfa01.
    
    Reason for revert: Potential culprit for b/263035249 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Change-Id: I903af90005c9b4374ba41394c753d2436f0b0ef3

--- services/audiopolicy/service/Spatializer.cpp
+++ services/audiopolicy/service/Spatializer.cpp
@@ -963,10 +963,7 @@ void Spatializer::checkSensorsState_l() {
         }
     }
     if (mOutput != AUDIO_IO_HANDLE_NONE && supportsSetLatencyMode) {
-        const status_t status =
-                AudioSystem::setRequestedLatencyMode(mOutput, requestedLatencyMode);
-        ALOGD("%s: setRequestedLatencyMode for output thread(%d) to %s returned %d",
-                __func__, mOutput, toString(requestedLatencyMode).c_str(), status);
+        AudioSystem::setRequestedLatencyMode(mOutput, requestedLatencyMode);
     }
 }
 
