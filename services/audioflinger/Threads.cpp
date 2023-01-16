25f6a3b9eac1c019a3c3479e9e52ffd44271246a
zhangjincheng <zhangjincheng@xiaomi.com>
2023-01-16 17:17:38 +0800

    audio: disable master audio balance for voip mode
    When modifying "Audio balance" to Right, the mMasterBalanceLeft
    will be set to 0.0.  The action will cause no sound on earpiece
    in voip calls.
    
    We think the master audio balance should be disabled when voip,
    only effect on nornal mode sound.
    
    Change-Id: I1ae010d7cd352bf7a9be251775d62c78fc15e1b0
    Signed-off-by: zhangjincheng <zhangjincheng@xiaomi.com>

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -6191,12 +6191,22 @@ void AudioFlinger::DirectOutputThread::processVolume_l(Track *track, bool lastTr
         if (left > GAIN_FLOAT_UNITY) {
             left = GAIN_FLOAT_UNITY;
         }
-        left *= v * mMasterBalanceLeft; // DirectOutputThread balance applied as track volume
+        if ((mAudioFlinger->getMode() == AUDIO_MODE_IN_COMMUNICATION) &&
+                isSingleDeviceType(outDeviceTypes(), AUDIO_DEVICE_OUT_EARPIECE)) {
+            left *= v;
+        } else {
+            left *= v * mMasterBalanceLeft; // DirectOutputThread balance applied as track volume
+        }
         right = float_from_gain(gain_minifloat_unpack_right(vlr));
         if (right > GAIN_FLOAT_UNITY) {
             right = GAIN_FLOAT_UNITY;
         }
-        right *= v * mMasterBalanceRight;
+        if ((mAudioFlinger->getMode() == AUDIO_MODE_IN_COMMUNICATION) &&
+                isSingleDeviceType(outDeviceTypes(), AUDIO_DEVICE_OUT_EARPIECE)) {
+            right *= v;
+        } else {
+            right *= v * mMasterBalanceRight;
+        }
     }
 
     if (lastTrack) {
