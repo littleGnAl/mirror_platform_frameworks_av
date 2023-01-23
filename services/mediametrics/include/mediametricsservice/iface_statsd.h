LineageOS/android_frameworks_av

IDrm metrics to statsd
Add statsd pushers
statsd_mediadrm_new, statsd_mediadrm_session, statsd_mediadrm_err
that log IDrm metrics to statsd in mediametrics.

Bug: 249941041
Test: m mediametrics
Change-Id: Ia0fdd28aa5935809a0e961ef3098bf9354dede40

services/mediametrics/include/mediametricsservice/iface_statsd.h
@@ -32,6 +32,9 @@ extern statsd_pusher statsd_audiotrack;
 extern statsd_pusher statsd_codec;
 extern statsd_pusher statsd_extractor;
 extern statsd_pusher statsd_mediaparser;
+extern statsd_pusher statsd_media_drm_created;
+extern statsd_pusher statsd_media_drm_session_opened;
+extern statsd_pusher statsd_media_drm_errored;
 
 extern statsd_pusher statsd_nuplayer;
 extern statsd_pusher statsd_recorder;

littleGnAl/mirror_platform_frameworks_av

services/mediametrics/include/mediametricsservice/iface_statsd.h
@@ -30,13 +30,15 @@ extern statsd_pusher statsd_audiorecord;
 extern statsd_pusher statsd_audiothread;
 extern statsd_pusher statsd_audiotrack;
 extern statsd_pusher statsd_codec;
+extern statsd_pusher statsd_drmmanager;
 extern statsd_pusher statsd_extractor;
+extern statsd_pusher statsd_mediadrm;
+extern statsd_pusher statsd_mediadrm_created;
+extern statsd_pusher statsd_mediadrm_errored;
+extern statsd_pusher statsd_mediadrm_session_opened;
 extern statsd_pusher statsd_mediaparser;
-
 extern statsd_pusher statsd_nuplayer;
 extern statsd_pusher statsd_recorder;
-extern statsd_pusher statsd_mediadrm;
-extern statsd_pusher statsd_drmmanager;
 
 using statsd_puller = bool (const std::shared_ptr<const mediametrics::Item>& item,
         AStatsEventList *, const std::shared_ptr<mediametrics::StatsdLog>& statsdLog);