1dd85c3c9daddc03490de6d3a09c0bf3698c26a8
Kwangkyu Park <kk48.park@samsung.com>
2023-02-10 21:23:09 +0900

    Implement camera stacktrace feature
    If camera watchdog bites, currently there is no stacktrace for a camera
    provider to debug with. By allowing cameraserver to talk with
    tomestoned, cameraserver will print out the stacktrace.
    
    Bug: 267702086
    Test: manual test after adding sleep() to trigger camera service
    watchdog, and check platform log for stacktraces
    
    Change-Id: Ib3f5a16604da5967002df59cbc95a164a9e2fea2

---
+++ services/camera/libcameraservice/utils/CameraTombstone.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "CameraTombstone"
+#define ATRACE_TAG ATRACE_TAG_CAMERA
+
+#include "utils/CameraTombstone.h"
+
+#include <utils/Log.h>
+
+namespace android {
+namespace camera3 {
+
+void CameraTombstone::dump(pid_t pid) {
+    ALOGV("%s: pid = %d", __FUNCTION__, pid);
+
+    base::unique_fd piperead, pipewrite;
+    if (!Pipe(&piperead, &pipewrite)) {
+        ALOGE("failed to create pipe");
+        return;
+    }
+
+    std::thread redirect_thread = spawn_redirect_thread(std::move(piperead));
+    if (!debuggerd_trigger_dump(pid, kDebuggerdNativeBacktrace, 0, std::move(pipewrite))) {
+        redirect_thread.join();
+        ALOGE("failed to dump process %d", pid);
+    }
+
+    redirect_thread.join();
+}
+
+std::thread CameraTombstone::spawn_redirect_thread(base::unique_fd fd) {
+    return std::thread([fd{std::move(fd)}]() {
+        std::string output;
+
+        while (true) {
+            char buf[BUFSIZ];
+            ssize_t rc = TEMP_FAILURE_RETRY(read(fd.get(), buf, sizeof(buf)));
+            if (rc <= 0) {
+                return;
+            }
+
+            output.append(buf, rc);
+            size_t pos = 0;
+            while ((pos = output.find('\n')) != std::string::npos) {
+                ALOGE("%s", output.substr(0, pos).c_str());
+                output.erase(0, pos + 1);
+            }
+        }
+
+        if (!output.empty()) {
+            ALOGE("%s", output.c_str());
+        }
+    });
+}
+
+};  // namespace camera3
+};  // namespace android
