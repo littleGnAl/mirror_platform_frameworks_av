211d5414c55e6d4ab6eb3583e4d48396aa86a9d9
guochuang <guochuang@xiaomi.corp-partner.google.com>
2023-06-07 21:14:50 +0800

    Remove duplicate include
    These headers are included twice
    
    Test: build & boot
    Change-Id: Ie89c1f2ee745d85b0542c502a0c2f2d33a4b6364
    Signed-off-by: guochuang <guochuang@xiaomi.corp-partner.google.com>

--- media/libstagefright/MediaCodec.cpp
+++ media/libstagefright/MediaCodec.cpp
@@ -31,7 +31,6 @@
 #include "include/SoftwareRenderer.h"
 #include "PlaybackDurationAccumulator.h"
 
-#include <android/binder_manager.h>
 #include <android/content/pm/IPackageManagerNative.h>
 #include <android/hardware/cas/native/1.0/IDescrambler.h>
 #include <android/hardware/media/omx/1.0/IGraphicBufferSource.h>
@@ -73,7 +72,6 @@
 #include <media/stagefright/MediaCodec.h>
 #include <media/stagefright/MediaCodecConstants.h>
 #include <media/stagefright/MediaCodecList.h>
-#include <media/stagefright/MediaCodecConstants.h>
 #include <media/stagefright/MediaDefs.h>
 #include <media/stagefright/MediaErrors.h>
 #include <media/stagefright/OMXClient.h>
