a5d20d53bb4bff7a24962b633876c14502eac395
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2023-07-01 03:48:20 +0000

    Revert "implementing concurrent codec metrics"
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/beeb60aa-4f17-4248-b6bd-cfb536895a55). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ia069910ec66fa8085015affe4de2bba506c5a8ba

--- services/mediaresourcemanager/ResourceManagerService.cpp
+++ services/mediaresourcemanager/ResourceManagerService.cpp
@@ -26,23 +26,30 @@
 #include <cutils/sched_policy.h>
 #include <dirent.h>
 #include <media/MediaResourcePolicy.h>
-#include <media/stagefright/foundation/ABase.h>
+#include <media/stagefright/ProcessInfo.h>
 #include <mediautils/BatteryNotifier.h>
-#include <mediautils/ProcessInfo.h>
 #include <mediautils/SchedulingPolicyService.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <stats_media_metrics.h>
 
 #include "IMediaResourceMonitor.h"
-#include "ResourceManagerMetrics.h"
 #include "ResourceManagerService.h"
 #include "ResourceObserverService.h"
 #include "ServiceLog.h"
 
 namespace android {
+
+using stats::media_metrics::stats_write;
+using stats::media_metrics::MEDIA_CODEC_RECLAIM_REQUEST_COMPLETED;
+using stats::media_metrics::MEDIA_CODEC_RECLAIM_REQUEST_COMPLETED__RECLAIM_STATUS__RECLAIM_SUCCESS;
+using stats::media_metrics::\
+    MEDIA_CODEC_RECLAIM_REQUEST_COMPLETED__RECLAIM_STATUS__RECLAIM_FAILED_NO_CLIENTS;
+using stats::media_metrics::\
+    MEDIA_CODEC_RECLAIM_REQUEST_COMPLETED__RECLAIM_STATUS__RECLAIM_FAILED_RECLAIM_RESOURCES;
 
 //static
 std::mutex ResourceManagerService::sCookieLock;
@@ -53,8 +60,8 @@ std::map<uintptr_t, sp<DeathNotifier> > ResourceManagerService::sCookieToDeathNo
 
 class DeathNotifier : public RefBase {
 public:
-    DeathNotifier(const std::shared_ptr<ResourceManagerService> &service,
-                  const ClientInfoParcel& clientInfo);
+    DeathNotifier(const std::shared_ptr<ResourceManagerService> &service, int pid,
+            int64_t clientId);
 
     virtual ~DeathNotifier() {}
 
@@ -64,12 +71,13 @@ public:
 
 protected:
     std::weak_ptr<ResourceManagerService> mService;
-    const ClientInfoParcel mClientInfo;
+    int mPid;
+    int64_t mClientId;
 };
 
 DeathNotifier::DeathNotifier(const std::shared_ptr<ResourceManagerService> &service,
-                             const ClientInfoParcel& clientInfo)
-    : mService(service), mClientInfo(clientInfo) {}
+        int pid, int64_t clientId)
+    : mService(service), mPid(pid), mClientId(clientId) {}
 
 //static
 void DeathNotifier::BinderDiedCallback(void* cookie) {
@@ -96,16 +104,16 @@ void DeathNotifier::binderDied() {
         return;
     }
 
-    service->overridePid(mClientInfo.pid, -1);
+    service->overridePid(mPid, -1);
     // thiz is freed in the call below, so it must be last call referring thiz
-    service->removeResource(mClientInfo, false /*checkValid*/);
+    ClientInfoParcel clientInfo{.pid = mPid, .id = mClientId};
+    service->removeResource(clientInfo, false /*checkValid*/);
 }
 
 class OverrideProcessInfoDeathNotifier : public DeathNotifier {
 public:
     OverrideProcessInfoDeathNotifier(const std::shared_ptr<ResourceManagerService> &service,
-                                     const ClientInfoParcel& clientInfo)
-            : DeathNotifier(service, clientInfo) {}
+            int pid) : DeathNotifier(service, pid, 0) {}
 
     virtual ~OverrideProcessInfoDeathNotifier() {}
 
@@ -120,7 +128,7 @@ void OverrideProcessInfoDeathNotifier::binderDied() {
         return;
     }
 
-    service->removeProcessInfoOverride(mClientInfo.pid);
+    service->removeProcessInfoOverride(mPid);
 }
 
 template <typename T>
@@ -193,11 +201,7 @@ static ResourceInfo& getResourceInfoForEdit(uid_t uid, int64_t clientId,
         ResourceInfo info;
         info.uid = uid;
         info.clientId = clientId;
-        if (name.empty()) {
-            info.name = "<unknown client>";
-        } else {
-            info.name = name;
-        }
+        info.name = name;
         info.client = client;
         info.cookie = 0;
         info.pendingRemoval = false;
@@ -287,7 +291,10 @@ binder_status_t ResourceManagerService::dump(int fd, const char** /*args*/, uint
             snprintf(buffer, SIZE, "        Id: %lld\n", (long long)infos[j].clientId);
             result.append(buffer);
 
-            std::string clientName = infos[j].name;
+            std::string clientName = "<unknown client>";
+            if (infos[j].client != nullptr) {
+                clientName = infos[j].name;
+            }
             snprintf(buffer, SIZE, "        Name: %s\n", clientName.c_str());
             result.append(buffer);
 
@@ -349,8 +356,6 @@ ResourceManagerService::ResourceManagerService(const sp<ProcessInfoInterface> &p
       mCpuBoostCount(0),
       mDeathRecipient(AIBinder_DeathRecipient_new(DeathNotifier::BinderDiedCallback)) {
     mSystemCB->noteResetVideo();
-    // Create ResourceManagerMetrics that handles all the metrics.
-    mResourceManagerMetrics = std::make_unique<ResourceManagerMetrics>(mProcessInfo);
 }
 
 //static
@@ -504,14 +509,47 @@ Status ResourceManagerService::addResource(const ClientInfoParcel& clientInfo,
     }
     if (info.cookie == 0 && client != nullptr) {
         info.cookie = addCookieAndLink_l(client,
-                new DeathNotifier(ref<ResourceManagerService>(), clientInfo));
+                new DeathNotifier(ref<ResourceManagerService>(), pid, clientId));
     }
     if (mObserverService != nullptr && !resourceAdded.empty()) {
         mObserverService->onResourceAdded(uid, pid, resourceAdded);
     }
     notifyResourceGranted(pid, resources);
 
+    // Increase the instance count of the resource associated with this client.
+    increaseResourceInstanceCount(clientId, name);
+
     return Status::ok();
+}
+
+void ResourceManagerService::increaseResourceInstanceCount(int64_t clientId,
+                                                           const std::string& name) {
+    // Check whether this client has been looked into already.
+    if (mClientIdSet.find(clientId) == mClientIdSet.end()) {
+        mClientIdSet.insert(clientId);
+        // Update the resource instance count.
+        auto found = mConcurrentResourceCountMap.find(name);
+        if (found == mConcurrentResourceCountMap.end()) {
+            mConcurrentResourceCountMap[name] = 1;
+        } else {
+            found->second++;
+        }
+    }
+}
+
+void ResourceManagerService::decreaseResourceInstanceCount(int64_t clientId,
+                                                           const std::string& name) {
+    // Since this client has been removed, remove it from mClientIdSet
+    mClientIdSet.erase(clientId);
+    // Update the resource instance count also.
+    auto found = mConcurrentResourceCountMap.find(name);
+    if (found != mConcurrentResourceCountMap.end()) {
+        if (found->second == 1) {
+            mConcurrentResourceCountMap.erase(found);
+        } else {
+            found->second--;
+        }
+    }
 }
 
 Status ResourceManagerService::removeResource(const ClientInfoParcel& clientInfo,
@@ -618,8 +656,9 @@ Status ResourceManagerService::removeResource(const ClientInfoParcel& clientInfo
         onLastRemoved(it->second, info);
     }
 
-    // Since this client has been removed, update the metrics collector.
-    mResourceManagerMetrics->notifyClientReleased(clientInfo);
+    // Since this client has been removed, decrease the corresponding
+    // resources instance count.
+    decreaseResourceInstanceCount(clientId, info.name);
 
     removeCookieAndUnlink_l(info.client, info.cookie);
 
@@ -751,19 +790,73 @@ Status ResourceManagerService::reclaimResource(const ClientInfoParcel& clientInf
 void ResourceManagerService::pushReclaimAtom(const ClientInfoParcel& clientInfo,
                         const Vector<std::shared_ptr<IResourceManagerClient>>& clients,
                         const PidUidVector& idVector, bool reclaimed) {
+    // Construct the metrics for codec reclaim as a pushed atom.
+    // 1. Information about the requester.
+    //  - UID and the priority (oom score)
     int32_t callingPid = clientInfo.pid;
+    int32_t requesterUid = clientInfo.uid;
+    std::string clientName = clientInfo.name;
     int requesterPriority = -1;
     getPriority_l(callingPid, &requesterPriority);
-    std::vector<int> priorities;
-    priorities.push_back(requesterPriority);
 
-    for (PidUidVector::const_reference id : idVector) {
+    //  2. Information about the codec.
+    //  - Name of the codec requested
+    //  - Number of concurrent codecs running.
+    int32_t noOfConcurrentCodecs = 0;
+    auto found = mConcurrentResourceCountMap.find(clientName);
+    if (found != mConcurrentResourceCountMap.end()) {
+        noOfConcurrentCodecs = found->second;
+    }
+
+    // 3. Information about the Reclaim:
+    // - Status of reclaim request
+    // - How many codecs are reclaimed
+    // - For each codecs reclaimed, information of the process that it belonged to:
+    //    - UID and the Priority (oom score)
+    int32_t reclaimStatus = MEDIA_CODEC_RECLAIM_REQUEST_COMPLETED__RECLAIM_STATUS__RECLAIM_SUCCESS;
+    if (!reclaimed) {
+      if (clients.size() == 0) {
+        // No clients to reclaim from
+        reclaimStatus =
+            MEDIA_CODEC_RECLAIM_REQUEST_COMPLETED__RECLAIM_STATUS__RECLAIM_FAILED_NO_CLIENTS;
+      } else {
+        // Couldn't reclaim resources from the clients
+        reclaimStatus =
+            MEDIA_CODEC_RECLAIM_REQUEST_COMPLETED__RECLAIM_STATUS__RECLAIM_FAILED_RECLAIM_RESOURCES;
+      }
+    }
+    int32_t noOfCodecsReclaimed = clients.size();
+    int32_t targetIndex = 1;
+    for (const auto& id : idVector) {
+        int32_t targetUid = id.second;
         int targetPriority = -1;
         getPriority_l(id.first, &targetPriority);
-        priorities.push_back(targetPriority);
+        // Post the pushed atom
+        int result = stats_write(
+            MEDIA_CODEC_RECLAIM_REQUEST_COMPLETED,
+            requesterUid,
+            requesterPriority,
+            clientName.c_str(),
+            noOfConcurrentCodecs,
+            reclaimStatus,
+            noOfCodecsReclaimed,
+            targetIndex,
+            targetUid,
+            targetPriority);
+        ALOGI("%s: Pushed MEDIA_CODEC_RECLAIM_REQUEST_COMPLETED atom: "
+              "Requester[pid(%d): uid(%d): priority(%d)] "
+              "Codec: [%s] "
+              "No of concurrent codecs: %d "
+              "Reclaim Status: %d "
+              "No of codecs reclaimed: %d "
+              "Target[%d][pid(%d): uid(%d): priority(%d)] "
+              "Atom Size: %d",
+              __func__, callingPid, requesterUid, requesterPriority,
+              clientName.c_str(), noOfConcurrentCodecs,
+              reclaimStatus, noOfCodecsReclaimed,
+              targetIndex, id.first, targetUid, targetPriority, result);
+        targetIndex++;
     }
-    mResourceManagerMetrics->pushReclaimAtom(clientInfo, priorities, clients,
-                                             idVector, reclaimed);
 }
 
 bool ResourceManagerService::reclaimUnconditionallyFrom(
@@ -839,7 +932,6 @@ Status ResourceManagerService::overridePid(int originalPid, int newPid) {
         mOverridePidMap.erase(originalPid);
         if (newPid != -1) {
             mOverridePidMap.emplace(originalPid, newPid);
-            mResourceManagerMetrics->addPid(newPid);
         }
     }
 
@@ -873,12 +965,8 @@ Status ResourceManagerService::overrideProcessInfo(
         return Status::fromServiceSpecificError(BAD_VALUE);
     }
 
-    ClientInfoParcel clientInfo{.pid = static_cast<int32_t>(pid),
-                                .uid = 0,
-                                .id = 0,
-                                .name = "<unknown client>"};
     uintptr_t cookie = addCookieAndLink_l(client,
-            new OverrideProcessInfoDeathNotifier(ref<ResourceManagerService>(), clientInfo));
+            new OverrideProcessInfoDeathNotifier(ref<ResourceManagerService>(), pid));
 
     mProcessInfoOverrideMap.emplace(pid, ProcessInfoOverride{cookie, client});
 
@@ -1191,29 +1279,6 @@ bool ResourceManagerService::getBiggestClient_l(int pid, MediaResource::Type typ
 
     *client = clientTemp;
     return true;
-}
-
-Status ResourceManagerService::notifyClientCreated(const ClientInfoParcel& clientInfo) {
-    mResourceManagerMetrics->notifyClientCreated(clientInfo);
-    return Status::ok();
-}
-
-Status ResourceManagerService::notifyClientStarted(const ClientConfigParcel& clientConfig) {
-    mResourceManagerMetrics->notifyClientStarted(clientConfig);
-    return Status::ok();
-}
-
-Status ResourceManagerService::notifyClientStopped(const ClientConfigParcel& clientConfig) {
-    mResourceManagerMetrics->notifyClientStopped(clientConfig);
-    return Status::ok();
-}
-
-long ResourceManagerService::getPeakConcurrentPixelCount(int pid) const {
-    return mResourceManagerMetrics->getPeakConcurrentPixelCount(pid);
-}
-
-long ResourceManagerService::getCurrentConcurrentPixelCount(int pid) const {
-    return mResourceManagerMetrics->getCurrentConcurrentPixelCount(pid);
 }
 
 } // namespace android
