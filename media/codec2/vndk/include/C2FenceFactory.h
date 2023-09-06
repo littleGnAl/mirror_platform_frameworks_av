7d864d0c3fb09e81ceed1f481ddc76f9e20e0442
Sungtak Lee <taklee@google.com>
2023-09-06 18:08:09 +0000

    media.c2 aidl: Implement eventfd based C2Fence
    Implement eventfd based C2Fence. The eventfd based C2Fence will be
    waitable object for C2BlockPool of media.c2 aidl.
    
    Bug: 254050314
    Change-Id: I77e6af77721cc289f7e6ae039cb757a12e374e94

--- media/codec2/vndk/include/C2FenceFactory.h
+++ media/codec2/vndk/include/C2FenceFactory.h
@@ -31,6 +31,7 @@
 std::vector<int> ExtractFdsFromCodec2SyncFence(const C2Fence& fence);
 
 class C2SurfaceSyncMemory;
+class C2IgbaWaitableObj;
 
 /**
  * C2Fence implementation factory
@@ -39,6 +40,7 @@ struct _C2FenceFactory {
 
     class SurfaceFenceImpl;
     class SyncFenceImpl;
+    class EventFenceImpl;
 
     /*
      * Create C2Fence for BufferQueueBased blockpool.
@@ -65,6 +67,15 @@ struct _C2FenceFactory {
      *                          It will be owned and closed by the returned fence object.
      */
     static C2Fence CreateMultipleFdSyncFence(const std::vector<int>& fenceFds);
+
+    /*
+     * Create C2Fence from fds which are created from eventfd().
+     *
+     * \param obj               Waitable object for C2AIDL IGraphicBufferAllocator.
+     *                          It will notify when it is ready to allocate.
+     *
+     */
+    static C2Fence CreateEventFence(const std::shared_ptr<C2IgbaWaitableObj> &obj);
 
     /**
      * Create a native handle from fence for marshalling
