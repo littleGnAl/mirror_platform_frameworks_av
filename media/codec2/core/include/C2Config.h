75439229ea4feceaa1d4844c948ad9bf63476fd6
Arun Johnson <arunjohnson@google.com>
2023-09-06 18:31:38 +0000

    Def: C2 large audio frame
    Bug: 298052174
    Change-Id: I8c3d956872c63ae73c8d4f9c012a6160f5e9fd47

--- media/codec2/core/include/C2Config.h
+++ media/codec2/core/include/C2Config.h
@@ -147,7 +147,7 @@ enum C2ParamIndexKind : C2Param::type_index_t {
     kParamIndexMaxBufferSize,
 
     /* large audio frame support */
-    kParamIndexThresholdBufferSize,
+    kParamIndexLargeFrameParams,
 
     /* misc. state */
     kParamIndexTripped,
@@ -237,6 +237,7 @@ enum C2ParamIndexKind : C2Param::type_index_t {
     kParamIndexDrcOutputLoudness, // drc, float (dBFS)
     kParamIndexDrcAlbumMode, // drc, enum
     kParamIndexAudioFrameSize, // int
+    kParamIndexLargeFrameMetadata, // struct
 
     /* ============================== platform-defined parameters ============================== */
 
@@ -1118,18 +1119,25 @@ typedef C2StreamParam<C2Info, C2Uint32Value, kParamIndexMaxBufferSize> C2StreamM
 constexpr char C2_PARAMKEY_INPUT_MAX_BUFFER_SIZE[] = "input.buffers.max-size";
 constexpr char C2_PARAMKEY_OUTPUT_MAX_BUFFER_SIZE[] = "output.buffers.max-size";
 
-/**
- * Threshold buffer size
- *
- * Specifies the threshold size in bytes of a buffer of data. The component may
- * return output buffers as it reaches this threshold size. This may be used as an optimization
- * and avoid memcpy while generating output buffers.
- *
- */
-typedef C2StreamParam<C2Tuning, C2Uint32Value, kParamIndexThresholdBufferSize>
-        C2StreamThresholdBufferSizeInfo;
-constexpr char C2_PARAMKEY_OUTPUT_THRESHOLD_BUFFER_SIZE[] = "output.buffers.threshold-size";
+struct C2LargeFrameParamsStruct {
+    uint32_t maxOutputSize;
+    uint32_t thresholdSize;
 
+    C2LargeFrameParamsStruct()
+        : maxOutputSize(0),
+          thresholdSize(0) {}
+
+    C2LargeFrameParamsStruct(uint32_t maxOutputSize_, uint32_t thresholdSize_)
+        : maxOutputSize(maxOutputSize_), thresholdSize(thresholdSize_) {}
+
+    DEFINE_AND_DESCRIBE_C2STRUCT(LargeFrameParams)
+    C2FIELD(maxOutputSize, "maxOutputSize")
+    C2FIELD(thresholdSize, "thresholdSize")
+};
+
+typedef C2StreamParam<C2Tuning, C2LargeFrameParamsStruct, kParamIndexLargeFrameParams>
+        C2LargeFrameParams;
+constexpr char C2_PARAMKEY_LARGE_FRAME_PARAMS[] = "output.largeframe.params";
 
 /* ---------------------------------------- misc. state ---------------------------------------- */
 
@@ -2162,6 +2170,48 @@ constexpr char C2_PARAMKEY_DRC_OUTPUT_LOUDNESS[] = "output.drc.output-loudness";
 typedef C2StreamParam<C2Info, C2Uint32Value, kParamIndexAudioFrameSize>
         C2StreamAudioFrameSizeInfo;
 constexpr char C2_PARAMKEY_AUDIO_FRAME_SIZE[] = "raw.audio-frame-size";
+
+/**
+ * Large audio frame support metadata
+ *
+ * When available, audio components may use this metadata to identify or indicate
+ * access-unit boundaries when multiple access-units are provided in a single
+ * input/output buffer.
+ */
+struct C2LargeFrameMetadataStruct {
+
+    inline C2LargeFrameMetadataStruct() {
+        memset(this, 0, sizeof(*this));
+    }
+
+    inline C2LargeFrameMetadataStruct(
+            uint32_t flags_,
+            uint64_t size_,
+            uint64_t offset_,
+            int64_t presentationTimeUs_)
+        : flags(flags_),
+          size(size_),
+          offset(offset_),
+          presentationTimeUs(presentationTimeUs_) { }
+
+    uint32_t flags; ///<flags for the access-unit
+    uint64_t size; ///<size of access-unit
+    uint64_t offset; ///<offset of access-unit within this buffer
+    int64_t presentationTimeUs; ///<presentationTime in us for each access-unit
+
+    DEFINE_AND_DESCRIBE_C2STRUCT(LargeFrameMetadata)
+    C2FIELD(flags, "flags")
+    C2FIELD(size, "size")
+    C2FIELD(offset, "offset")
+    C2FIELD(presentationTimeUs, "presentationTimeUs")
+};
+
+typedef C2StreamParam<C2Info, C2SimpleArrayStruct<C2LargeFrameMetadataStruct>,
+                kParamIndexLargeFrameMetadata>
+        C2LargeFrameMetadata;
+
+constexpr char C2_PARAMKEY_INPUT_LARGEFRAME_METADATA[] = "input.large-frame-metadata";
+constexpr char C2_PARAMKEY_OUTPUT_LARGEFRAME_METADATA[] = "output.large-frame-metadata";
 
 /* --------------------------------------- AAC components --------------------------------------- */
 
