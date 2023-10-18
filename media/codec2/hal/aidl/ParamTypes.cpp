7b941678002851f4065c8c559140fd762860d9b8
Wonsik Kim <wonsik@google.com>
2023-10-18 16:09:47 -0700

    Add an aconfig flag for media.c2 AIDL impl
    Bug: 251850069
    Test: adb shell dumpsys android.hardware.media.c2.IComponentStore/software
    Change-Id: Icc93b397fbb6d72dbf11775eb1404caf02cf3409

--- media/codec2/hal/aidl/ParamTypes.cpp
+++ media/codec2/hal/aidl/ParamTypes.cpp
@@ -19,9 +19,9 @@
 #include <android-base/logging.h>
 
 #include <android/binder_manager.h>
+#include <android-base/properties.h>
 // NOTE: due to dependency from mainline modules cannot use libsysprop
 // #include <android/sysprop/MediaProperties.sysprop.h>
-#include <android-base/properties.h>
 #include <apex/AMediaFlags.h>
 #include <codec2/aidl/ParamTypes.h>
 #include <codec2/common/ParamTypes.h>
@@ -162,11 +162,16 @@ namespace media {
 namespace c2 {
 namespace utils {
 
-bool IsSelected() {
-    // TODO: read from aconfig flags
-    const bool enabled = false;
+bool IsEnabled() {
+    if (__builtin_available(android __ANDROID_API_V__, *)) {
+        return ::AMediaFlags_isCodec2AidlEnabled();
+    } else {
+        return false;
+    }
+}
 
-    if (!enabled) {
+bool IsSelected() {
+    if (!IsEnabled()) {
         // Cannot select AIDL if not enabled
         return false;
     }
