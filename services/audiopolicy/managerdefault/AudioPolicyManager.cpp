LineageOS/android_frameworks_av

commit caafba4078dc869b3686c24e7046319550189f27

audio policy: fix virtual called from constructor
Call initialize() after constructing AudioPolicyManager
from factory to avoid calling virtual methods from constructor.

Bug: 136491567
Test: make
Change-Id: Ie0b591d90e643b1fd1c326f9731dc656b38230dc

services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -4306,7 +4306,6 @@ AudioPolicyManager::AudioPolicyManager(AudioPolicyClientInterface *clientInterfa
         : AudioPolicyManager(clientInterface, false /*forTesting*/)
 {
     loadConfig();
-    initialize();
 }
 
 void AudioPolicyManager::loadConfig() {

services/audiopolicy/manager/AudioPolicyFactory.cpp
@@ -21,7 +21,13 @@ namespace android {
 extern "C" AudioPolicyInterface* createAudioPolicyManager(
         AudioPolicyClientInterface *clientInterface)
 {
-    return new AudioPolicyManager(clientInterface);
+    AudioPolicyManager *apm = new AudioPolicyManager(clientInterface);
+    status_t status = apm->initialize();
+    if (status != NO_ERROR) {
+        delete apm;
+        apm = nullptr;
+    }
+    return apm;
 }
 
 extern "C" void destroyAudioPolicyManager(AudioPolicyInterface *interface)

services/audiopolicy/managerdefault/AudioPolicyManager.h
@@ -307,6 +307,8 @@ public:
             return volumeGroup != VOLUME_GROUP_NONE ? NO_ERROR : BAD_VALUE;
         }
 
+        status_t initialize();
+
 protected:
         // A constructor that allows more fine-grained control over initialization process,
         // used in automatic tests.
@@ -321,7 +323,6 @@ protected:
         //   - initialize.
         AudioPolicyConfig& getConfig() { return mConfig; }
         void loadConfig();
-        status_t initialize();
 
         // From AudioPolicyManagerObserver
         virtual const AudioPatchCollection &getAudioPatches() const

littleGnAl/mirror_platform_frameworks_av

commit 02e7ef927c601d997865758ced8ccd6faf935031

audiopolicy/managerdefault: Allow non-attached default output devices
Some embedded devices, like set-top boxes, may not have attached
output audio devices at all. And it's common for them to have an HDMI
port (and maybe Bluetooth) as the only option to output audio. And
it's natural for them to use a non-attached output device as the
defaultOutputDevice.

Unfortunately, the default APM doesn't allow this. To initialize APM,
the defaultOutputDevice should be available. For non-attached devices,
it means that the device state should be 'connected', but it's
impossible to change the device connection state, because APM is not
initialized yet.

Until Android 10, the result of AndroidPolicyManager::initialize() was
simply ignored. So it was possible to use HDMI (and other non-attached
output devices) as the defaultOutputDevice.

The breaking change was introduced in commit caafba4078 ("audio
policy: fix virtual called from constructor").

Test: manual - set non-attached device as defaultOutputDevice and run
stop audioserver && start audioserver

Change-Id: Idd2ad960d80ae3587c162fd75cfb333de47183ec
Signed-off-by: Sergey Bobrenok <SIBobrenok@sberdevices.ru>

services/audiopolicy/managerdefault/AudioPolicyManager.cpp
@@ -6054,7 +6054,8 @@ status_t AudioPolicyManager::initialize() {
             !mAvailableOutputDevices.contains(defaultOutputDevice)) {
         ALOGE_IF(defaultOutputDevice != nullptr, "Default device %s is unreachable",
                  defaultOutputDevice->toString().c_str());
-        status = NO_INIT;
+        // Some types of devices (e.g. set-top boxes) can have no attached audio at all.
+        // In this case it's not an error
     }
     ALOGW_IF(mPrimaryOutput == nullptr, "The policy configuration does not declare a primary output");
 