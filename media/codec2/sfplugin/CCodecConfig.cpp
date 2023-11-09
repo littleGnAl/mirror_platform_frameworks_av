4e75bafaa386e56f74486783e22c4c7f02c817af
Arun Johnson <arunjohnson@google.com>
2023-11-09 21:22:48 +0000

    Large audio frames in MediaCodec
    Bug: 298052174
    
    Change-Id: Ie091c82a102afce908d703278522cbac4cd3bb63

--- media/codec2/sfplugin/CCodecConfig.cpp
+++ media/codec2/sfplugin/CCodecConfig.cpp
@@ -402,9 +402,18 @@ void CCodecConfig::initializeStandardParams() {
 
     add(ConfigMapper(KEY_MAX_INPUT_SIZE, C2_PARAMKEY_INPUT_MAX_BUFFER_SIZE, "value")
         .limitTo(D::INPUT));
+
     // remove when codecs switch to PARAMKEY
     deprecated(ConfigMapper(KEY_MAX_INPUT_SIZE, "coded.max-frame-size", "value")
                .limitTo(D::INPUT));
+
+    // large frame params
+    add(ConfigMapper(KEY_MAX_BUFFER_BATCH_OUTPUT_SIZE,
+            C2_PARAMKEY_OUTPUT_LARGE_FRAME, "max-size")
+        .limitTo(D::AUDIO & D::OUTPUT));
+    add(ConfigMapper(KEY_THRESHOLD_BUFFER_BATCH_OUTPUT_SIZE,
+            C2_PARAMKEY_OUTPUT_LARGE_FRAME, "threshold")
+        .limitTo(D::AUDIO & D::OUTPUT));
 
     // Rotation
     // Note: SDK rotation is clock-wise, while C2 rotation is counter-clock-wise
