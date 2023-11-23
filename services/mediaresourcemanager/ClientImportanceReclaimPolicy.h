4b1d653e3e6cbe27baec155a19d15b4b9e4c58da
Girish <girishshetty@google.com>
2023-11-23 11:23:07 +0000

    resourcemanager: importance based resource reclaim
    Extend the reclaim policy by implementing codec importance
    based resource reclaim.
    User (of the codec) will assign codec importance to
    distinguish important codecs from less important.
    Upon resource conflict, the Resource Manager Service
    reclaims a codec with the lowest importance (lower than that of
    the requesting codec) from the same process (if there is one).
    
    Bug: 289097671
    Test: atest android.media.misc.cts.ResourceManagerTest
          atest android.media.misc.cts.ResourceManagerMultiTest
          /data/nativetest64/ResourceManagerService_test/ResourceManagerService_test
          /data/nativetest64/ResourceObserverService_test/ResourceObserverService_test
    Change-Id: I383d88c852edae53d3eca24d7b563f133a63acaa

---
+++ services/mediaresourcemanager/ClientImportanceReclaimPolicy.h
@@ -0,0 +1,85 @@
+/*
+**
+** Copyright 2023, The Android Open Source Project
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+#ifndef ANDROID_MEDIA_CLIENTIMPORTANCERECLAIMPOLICY_H_
+#define ANDROID_MEDIA_CLIENTIMPORTANCERECLAIMPOLICY_H_
+
+#include <media/MediaResource.h>
+#include "IReclaimPolicy.h"
+
+namespace android {
+
+class ResourceTracker;
+struct ClientInfo;
+
+/*
+ * Implementation of Reclaim Policy based on the client's importance.
+ *
+ * Find the lowest important (than that of requesting client) client from the
+ * same process (that is requesting for the resource).
+ * If there are multiple clients of lowest importance, then pick the biggest
+ * client.
+ *
+ * NOTE that this reclaim policy is used as a secondraory policy.
+ * So, it is not required to address the codec conflict here as it
+ * has been addressed by the Resource Model and the primary Reclaim Policy
+ * already.
+ */
+class ClientImportanceReclaimPolicy : public IReclaimPolicy {
+public:
+    ClientImportanceReclaimPolicy(const std::shared_ptr<ResourceTracker>& resourceTracker);
+
+    virtual ~ClientImportanceReclaimPolicy();
+
+    /*
+     * Based on the client importance, identify and return a client from the list
+     * of given clients that satisfy the resource requested.
+     *
+     * @param[in]  reclaimRequestInfo Information about the resource request
+     * @param[in]  client List of clients to select from.
+     * @param[out] targetClients Upon success, this will have the list of identified client(s).
+     *
+     * @return true on success, false otherwise
+     */
+    bool getClients(const ReclaimRequestInfo& reclaimRequestInfo,
+                    const std::vector<ClientInfo>& clients,
+                    std::vector<ClientInfo>& targetClients) override;
+
+private:
+
+    // Get the biggest client with the given resources from the given list of clients.
+    // The client should have lowest possible importance than that of the
+    // requesting client's importance.
+    // returns true on success, false otherwise
+    //
+    bool getBiggestClientFromLowestPriority(
+        pid_t callingPid,
+        int callingPriority,
+        MediaResource::Type type,
+        MediaResource::SubType subType,
+        MediaResource::SubType primarySubType,
+        const std::vector<ClientInfo>& clients,
+        ClientInfo& targetClient,
+        int& lowestPriority);
+
+private:
+    std::shared_ptr<ResourceTracker> mResourceTracker;
+};
+
+} // namespace android
+
+#endif  // ANDROID_MEDIA_CLIENTIMPORTANCERECLAIMPOLICY_H_
