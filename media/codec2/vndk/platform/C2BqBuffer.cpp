84b7480a0eb7d0cde9b7b90c77eb9b9f6cfd8655
Sungtak Lee <taklee@google.com>
2023-12-04 07:25:34 +0000

    Codec2 vndk: clear pending lock by a dead client process
    Bug: 312707275
    Bug: 309898317
    Bug: 304183768
    Change-Id: I5c6c2bbdd4859220a3f41f83998d88d0a7b9ddcb

--- media/codec2/vndk/platform/C2BqBuffer.cpp
+++ media/codec2/vndk/platform/C2BqBuffer.cpp
@@ -790,6 +790,13 @@ public:
         if (oldVar) {
             oldVar->invalidate();
         }
+        // invalidate pending lock from a dead process if any
+        if (syncVar) {
+            syncVar->clearLockIfNecessary();
+        }
+        if (oldVar) {
+            oldVar->clearLockIfNecessary();
+        }
     }
 
 private:
