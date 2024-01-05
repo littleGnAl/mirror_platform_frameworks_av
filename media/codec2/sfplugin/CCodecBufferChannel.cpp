c99b5c518cd3df76170068535bbd603a7144a94f
Arun Johnson <arunjohnson@google.com>
2024-01-05 21:16:56 +0000

    Change to output ArrayMode if using SkipCutBuffer
     - SkipCutBuffer requires write access so we need to Change the
       buffers to ArrayMode and get the buffers with write access.
    
    Bug: 316303561
    Change-Id: I24b00daa38b2b4ef2939a8e4f78166b3ce02dd6c

--- media/codec2/sfplugin/CCodecBufferChannel.cpp
+++ media/codec2/sfplugin/CCodecBufferChannel.cpp
@@ -1639,6 +1639,9 @@ status_t CCodecBufferChannel::start(
                 if (delay || padding) {
                     // We need write access to the buffers, and we're already in
                     // array mode.
+                    if (!output->buffers->isArrayMode()) {
+                        output->buffers = output->buffers->toArrayMode(numOutputSlots);
+                    }
                     output->buffers->initSkipCutBuffer(delay, padding, sampleRate, channelCount);
                 }
             }
